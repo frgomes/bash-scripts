95f3e5600e4a0b1bc21842b10d18dbeb94b13c66
Richard Gomes <rgomes.info@gmail.com>
2016-07-16 03:04:08 +0100

    Scala Language Specification 2.11

---
+++ install-scala_lang_spec_2.11.sh
@@ -0,0 +1,6 @@
+    $ sudo apt-get install httrack -y
+    $ hash -r
+    $ mkdir $HOME/Downloads/sls_2.11
+    $ cd $HOME/Downloads/sls_2.11
+    $ httrack http://www.scala-lang.org/files/archive/spec/2.11
+    
