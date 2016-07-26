09839a9408ba86c64247f4d2670ddd28e18e01b4
Richard Gomes <rgomes.info@gmail.com>
2016-07-26 23:06:25 +0100

    Define a shortcut for xrandr, instead of running xrandr at startup.

--- bash_05display.sh
+++ bash_05display.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-xrandr --output CRT1 --mode 1680x1050 --pos 0x0 --rotate normal --output DFP2 --mode 1920x1200 --pos 1680x0 --rotate normal --output DFP1 --off
+
+function xrandr_home {
+  xrandr --output CRT1 --mode 1680x1050 --pos 0x0 --rotate normal --output DFP2 --mode 1920x1200 --pos 1680x0 --rotate normal --output DFP1 --off
+}
