863689c0186c1fd6d93bc4a85e68566f599c6034
Richard Gomes <rgomes.info@gmail.com>
2016-07-27 10:27:06 +0100

    autoload Star environment at startup.

--- bash_60star.sh
+++ bash_60star.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 if [ -d ~/Development/repository ] ;then
+
   function branch() {
     local dir=$1
     local dir=${dir:=Trunk}
     if [ -d ~/Development/repository/${dir} ] ;then
+      # change directory to requested branch
       cd ~/Development/repository/${dir}
-      if [ -f Star5/star_functions.sh ] ;then
-        source Star5/star_functions.sh
+      # autoload utility functions
+      if [ -f $(pwd)/Star5/star_functions.sh ] ;then
+        source $(pwd)/Star5/star_functions.sh
       fi
     fi
   }
+
+  # autoload utility functions if activated from a branch
+  if [ -f $(pwd)/Star5/star_functions.sh ] ;then
+    source $(pwd)/Star5/star_functions.sh
+  fi
+
 fi
