5a6765478febf1e8298880a7562930706f3646c8
Richard Gomes <rgomes.info@gmail.com>
2016-08-06 18:56:12 +0100

    Remove function which never demonstrated to be useful or common.

--- bash_50scala.sh
+++ bash_50scala.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-function scala_expand_documentation() {
-  for base in /opt/developer/DOCS/scala-docs-* ;do
-    pushd $base/api > /dev/null 2>&1
-    pwd
-    # expand documentation from JAR files
-    ls jars | \
-      while read f ;do
-        dir=$( echo $f | sed 's/-javadoc.jar//' )
-        echo "    $dir"
-        mkdir -p $dir
-        pushd $dir > /dev/null 2>&1
-        jar -xf ../jars/$f
-        popd > /dev/null 2>&1
-      done
-    # expand documentation from .JAR files
-    rm -r -f scala-library scala-compiler scala-reflect
-    popd > /dev/null 2>&1
-  done
-}
+##########################
+## Scala and SBT utilities
+##########################
 
 
 function sbt_lazyci {
