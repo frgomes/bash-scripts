b7788b75781409b61a28b9796472620301bb5177
Richard Gomes <rgomes.info@gmail.com>
2016-08-06 18:53:28 +0100

    Introduce poorman's lazy continuous integration.

--- bash_50scala.sh
+++ bash_50scala.sh
@@ -19,3 +19,25 @@ function scala_expand_documentation() {
     popd > /dev/null 2>&1
   done
 }
+
+
+function sbt_lazyci {
+  function sbt_lazyci_git { while true ;do sleep 600; git pull; done }
+  function sbt_lazyci_hg  { while true ;do sleep 600; hg  pull; done }
+
+  function sbt_continuous_tests {
+    if [ -f ./sbt ] ;then
+      ./sbt clean ~test
+    else
+      sbt clean ~test
+    fi
+  }
+
+  if [ -d .git ] ;then
+    sbt_lazyci_git &
+    sbt_continuous_tests
+  elif [ -d .hg ] ;then
+    sbt_lazyci_hg &
+    sbt_continuous_tests
+  fi
+}
