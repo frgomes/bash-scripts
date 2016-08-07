ad8c57a5f9a7dc1e761b8f885489c37d2b2583bd
Richard Gomes <rgomes.info@gmail.com>
2016-08-07 16:43:42 +0100

    Consolidate git and mercurial in bash_22scm.sh

--- bash_22scm.sh
+++ bash_22scm.sh
@@ -1,5 +1,76 @@
 #!/bin/bash
 
+##################################
+## Utilities for git and mercurial
+##################################
+
+
+##
+## mercurial
+##
+
+
+function hg_filter {
+  while read line ;do
+    s="$( echo $line | cut -d' ' -f 1 )"
+    f="$( echo $line | cut -d' ' -f 2 )"
+    # echo ":::$s:::$f:::"
+    # if [ "$s" == "M" -o "$s" == "A" -o "$s" == "?" ] ;then
+    if [ -f "$f" ] ;then
+      echo $f
+    fi
+  done
+}
+
+
+function hg_changeset {
+
+  here=$( pwd )
+  name=$( basename $here )
+  dir=~/backup/${SUDO_USER:=${USER}}/changesets${here}
+
+  mkdir -p $dir
+
+  now=$(date +%Y%m%d_%H%M%S )
+  file=${dir}/${now}-${name}.tgz
+
+  hg status | hg_filter | xargs tar cpzf $file && echo $file
+}
+
+
+##
+## git
+##
+
+
+function git_filter {
+  while read line ;do
+    s="$( echo $line | cut -d' ' -f 1 )"
+    f="$( echo $line | cut -d' ' -f 2 )"
+    # echo ":::$s:::$f:::"
+    # if [ "$s" == "M" -o "$s" == "A" -o "$s" == "?" ] ;then
+    if [ -f "$f" ] ;then
+      echo $f
+    fi
+  done
+}
+
+
+function git_changeset {
+
+  here=$( pwd )
+  name=$( basename $here )
+  dir=~/backup/${SUDO_USER:=${USER}}/changesets${here}
+
+  mkdir -p $dir
+
+  now=$(date +%Y%m%d_%H%M%S )
+  file=${dir}/${now}-${name}.tgz
+
+  git status --porcelain | git_filter | xargs tar cpzf $file && echo $file
+}
+
+
 function git_origin_ssh {
   url=$(git config --get remote.origin.url)
   prj=$(basename $url)
@@ -7,6 +78,7 @@ function git_origin_ssh {
   git remote remove origin
   git remote add origin git@github.com:$owner/$prj.git
 }
+
 
 function git_clone {
     module=$1
@@ -30,6 +102,7 @@ function git_clone {
     git status
     popd
 }
+
 
 function git_switch {
     module=$1
