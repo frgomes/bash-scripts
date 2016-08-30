68f2dc5e75c78b52ca054d0e6960d3065ff1720c
Richard Gomes <rgomes@mathminds.io>
2016-08-30 23:15:59 +0100

    Minor improvements.

--- bash_50scala.sh
+++ bash_50scala.sh
@@ -4,30 +4,39 @@
 ## Scala and SBT utilities
 ##########################
 
+function sbt_runner {
+  if [ -f ./sbt ] ;then
+    ./sbt ${cmds}
+  else
+    sbt ${cmds}
+  fi
+}
 
-function sbt_lazy_ci {
-  function sbt_lazy_ci_git { while true ;do sleep 900; git pull; done }
-  function sbt_lazy_ci_hg  { while true ;do sleep 900; hg  pull --update; done }
+function sbt_lazyci {
+  function sbt_lazyci_git { while true ;do sleep 900; git pull; done }
+  function sbt_lazyci_hg  { while true ;do sleep 900; hg  pull --update; done }
 
-  function sbt_lazy_ci_tests {
+  function sbt_lazyci_tests {
     cmds=$*
     if [ ${#cmds} -eq 0 ] ;then
       cmds="clean test"
     fi
-
-    if [ -f ./sbt ] ;then
-      ./sbt ${cmds}
-    else
-      sbt ${cmds}
-    fi
+    sbt_runner ${cmds}
   }
 
-
   if [ -d .git ] ;then
-    sbt_lazy_ci_git &
-    sbt_lazy_ci_tests $*
+    sbt_lazyci_git &
+    sbt_lazyci_tests $*
   elif [ -d .hg ] ;then
-    sbt_lazy_ci_hg &
-    sbt_lazy_ci_tests $*
+    sbt_lazyci_hg &
+    sbt_lazyci_tests $*
   fi
+}
+
+function sbt_offline {
+   sbt "set offline:=true" $*
+}
+
+function sbt_online {
+   sbt_runner "set offline:=false" $*
 }
