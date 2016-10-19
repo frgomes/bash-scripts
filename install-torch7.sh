7a1af325e2d390d731a492ce54c7c3cf2a32ea4d
Richard Gomes <rgomes.info@gmail.com>
2016-10-19 22:11:19 +0100

    Torch7 installation needs libjpeg-dev

--- install-torch7.sh
+++ install-torch7.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Easy installation of Torch7 for Debian Jessie
 # Credits: http://github.com/geco/ezinstall
 sudo apt-get install lua5.2 liblua5.2-dev -y 
 sudo apt-get install qt4-default qt4-dev-tools -y
+sudo apt-get install libjpeg-dev -y
 curl -s https://raw.githubusercontent.com/geco/ezinstall/patch-1/install-all | sudo bash
