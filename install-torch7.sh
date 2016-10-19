d20446ca9c9d3c93ecabf0107e9408f3697e9b15
Richard Gomes <rgomes.info@gmail.com>
2016-10-19 22:54:33 +0100

    Install openblas and libreadline

--- install-torch7.sh
+++ install-torch7.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 # Easy installation of Torch7 for Debian Jessie
 # Credits: http://github.com/geco/ezinstall
-sudo apt-get install lua5.2 liblua5.2-dev -y 
-sudo apt-get install qt4-default qt4-dev-tools -y
-sudo apt-get install libjpeg-dev -y
-curl -s https://raw.githubusercontent.com/geco/ezinstall/patch-1/install-all | sudo bash
+
+sudo apt-get install qt4-default qt4-dev-tools libjpeg-dev libopenblas-dev libreadline-dev -y && \
+  curl -s https://raw.githubusercontent.com/geco/ezinstall/patch-1/install-all | sudo bash
