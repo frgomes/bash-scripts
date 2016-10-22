f1910c92f5139ff5d136b57706d221a164c96583
Richard Gomes <rgomes.info@gmail.com>
2016-10-22 01:08:44 +0100

    Docker instance startup functions.

--- bash_40docker.sh
+++ bash_40docker.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+function docker_torch7 {
+    docker run -it --rm -p 18888:8888 -v $HOME/workspace:/root/workspace rgomes/jessie-torch7
+}
+
 function docker_kdenlive {
-  XSOCK=/tmp/.X11-unix
-  XAUTH=/tmp/.docker.xauth
-  xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-  [[ ! -d $HOME/kdenlive ]] && mkdir -p $HOME/kdenlive
-  docker run -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH \
-             -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH \
-             -v $HOME/kdenlive:$HOME/kdenlive -v $HOME/workspace:$HOME/workspace \
-             -it --rm -u $USER \
-             rgomes/stretch-kdenlive
+    XSOCK=/tmp/.X11-unix
+    XAUTH=/tmp/.docker.xauth
+    xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+    [[ ! -d $HOME/kdenlive ]] && mkdir -p $HOME/kdenlive
+    docker run -it --rm \
+               -e DISPLAY=$DISPLAY \
+               -e XAUTHORITY=$XAUTH \
+               -v $XSOCK:$XSOCK \
+               -v $XAUTH:$XAUTH \
+               -v $HOME/kdenlive/:/root/kdenlive/ \
+               -v /media/b1582a88-e1e6-413d-bcac-881206487eb6/:/root/Media/ \
+               rgomes/stretch-kdenlive
 }
