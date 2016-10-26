ff0542af0f37ca2da837de4055fd6a26da85f7e8
Richard Gomes <rgomes.info@gmail.com>
2016-10-26 02:29:58 +0100

    Use TightVNC instead of XRDP.

--- bash_40docker.sh
+++ bash_40docker.sh
@@ -14,57 +14,57 @@ function docker_stretch_base {
 }
 
 function docker_stretch_kde_minimal {
-  # You may or may not have a xrdp already installed and kicking in your host computer.
+  # You may or may not have a TightVNC already installed and kicking in your host computer.
   # In a clean setup, you should not. However, you may be in a transitioning state which
   # implies that your host computer still have a lot of things installed which should not
   # be installed in a clean container host. In this case, we should employ something
-  # different from the default port 3389.
-  XRDP_PORT=53389
+  # different from the default port 5901.
+  VNC_PORT=55901
   docker run -it --rm \
-             -p ${XRDP_PORT}:3389 \
+             -p ${VNC_PORT}:5901 \
              -v ${HOME}/Documents:/root/Documents \
              -v ${HOME}/Media:/root/Media \
              rgomes/stretch-kde-minimal $*
 }
 
 function docker_stretch_kde_standard {
-  # You may or may not have a xrdp already installed and kicking in your host computer.
+  # You may or may not have a TightVNC already installed and kicking in your host computer.
   # In a clean setup, you should not. However, you may be in a transitioning state which
   # implies that your host computer still have a lot of things installed which should not
   # be installed in a clean container host. In this case, we should employ something
-  # different from the default port 3389.
-  XRDP_PORT=53389
+  # different from the default port 5901.
+  VNC_PORT=55901
   docker run -it --rm \
-             -p ${XRDP_PORT}:3389 \
+             -p ${VNC_PORT}:5901 \
              -v ${HOME}/Documents:/root/Documents \
              -v ${HOME}/Media:/root/Media \
              rgomes/stretch-kde-standard $*
 }
 
 function docker_stretch_kde_full {
-  # You may or may not have a xrdp already installed and kicking in your host computer.
+  # You may or may not have a TightVNC already installed and kicking in your host computer.
   # In a clean setup, you should not. However, you may be in a transitioning state which
   # implies that your host computer still have a lot of things installed which should not
   # be installed in a clean container host. In this case, we should employ something
-  # different from the default port 3389.
-  XRDP_PORT=53389
+  # different from the default port 5901.
+  VNC_PORT=55901
   docker run -it --rm \
-             -p ${XRDP_PORT}:3389 \
+             -p ${VNC_PORT}:5901 \
              -v ${HOME}/Documents:/root/Documents \
              -v ${HOME}/Media:/root/Media \
              rgomes/stretch-kde-full $*
 }
 
 function docker_stretch_kdenlive {
-  # You may or may not have a xrdp already installed and kicking in your host computer.
+  # You may or may not have a TightVNC already installed and kicking in your host computer.
   # In a clean setup, you should not. However, you may be in a transitioning state which
   # implies that your host computer still have a lot of things installed which should not
   # be installed in a clean container host. In this case, we should employ something
-  # different from the default port 3389.
-  XRDP_PORT=53389
+  # different from the default port 5901.
+  VNC_PORT=55901
   [[ ! -d $HOME/kdenlive ]] && mkdir -p $HOME/kdenlive
   docker run -it --rm \
-             -p ${XRDP_PORT}:3389 \
+             -p ${VNC_PORT}:5901 \
              -v ${HOME}/kdenlive:/root/kdenlive \
              -v ${HOME}/Documents:/root/Documents \
              -v ${HOME}/Media:/root/Media \
