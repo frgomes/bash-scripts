11b00b58d15f736d43b3f4e27f05f8827774e193
Richard Gomes <rgomes.info@gmail.com>
2016-10-27 00:29:17 +0100

    Remove not needed functions.

--- bash_40docker.sh
+++ bash_40docker.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+DOCKER_JUPYTER_PORT=18888
+DOCKER_VNC_PORT=55901
+
 function docker_jessie_torch7 {
     docker run -it --rm \
-	       -p 18888:8888 \
+	       -p ${DOCKER_JUPYTER_PORT}:8888 \
                -v ${HOME}/Documents:/root/Documents \
                -v ${HOME}/Media:/root/Media \
                -v ${HOME}/workspace:/root/workspace \
@@ -12,61 +15,40 @@ function docker_jessie_torch7 {
 function docker_stretch_base {
   docker run -it --rm rgomes/stretch-base $*
 }
-
-function docker_stretch_kde_minimal {
-  # You may or may not have a TightVNC already installed and kicking in your host computer.
-  # In a clean setup, you should not. However, you may be in a transitioning state which
-  # implies that your host computer still have a lot of things installed which should not
-  # be installed in a clean container host. In this case, we should employ something
-  # different from the default port 5901.
-  VNC_PORT=55901
-  docker run -it --rm \
-             -p ${VNC_PORT}:5901 \
-             -v ${HOME}/Documents:/root/Documents \
-             -v ${HOME}/Media:/root/Media \
-             rgomes/stretch-kde-minimal $*
+function docker_sid_base {
+  docker run -it --rm rgomes/sid-base $*
 }
 
-function docker_stretch_kde_standard {
-  # You may or may not have a TightVNC already installed and kicking in your host computer.
-  # In a clean setup, you should not. However, you may be in a transitioning state which
-  # implies that your host computer still have a lot of things installed which should not
-  # be installed in a clean container host. In this case, we should employ something
-  # different from the default port 5901.
-  VNC_PORT=55901
+function docker_sid_kde_minimal {
   docker run -it --rm \
-             -p ${VNC_PORT}:5901 \
+             -p ${DOCKER_VNC_PORT}:5901 \
              -v ${HOME}/Documents:/root/Documents \
              -v ${HOME}/Media:/root/Media \
-             rgomes/stretch-kde-standard $*
+             rgomes/sid-kde-minimal $*
 }
 
-function docker_stretch_kde_full {
-  # You may or may not have a TightVNC already installed and kicking in your host computer.
-  # In a clean setup, you should not. However, you may be in a transitioning state which
-  # implies that your host computer still have a lot of things installed which should not
-  # be installed in a clean container host. In this case, we should employ something
-  # different from the default port 5901.
-  VNC_PORT=55901
+function docker_sid_kde_standard {
   docker run -it --rm \
-             -p ${VNC_PORT}:5901 \
+             -p ${DOCKER_VNC_PORT}:5901 \
              -v ${HOME}/Documents:/root/Documents \
              -v ${HOME}/Media:/root/Media \
-             rgomes/stretch-kde-full $*
+             rgomes/sid-kde-standard $*
 }
 
-function docker_stretch_kdenlive {
-  # You may or may not have a TightVNC already installed and kicking in your host computer.
-  # In a clean setup, you should not. However, you may be in a transitioning state which
-  # implies that your host computer still have a lot of things installed which should not
-  # be installed in a clean container host. In this case, we should employ something
-  # different from the default port 5901.
-  VNC_PORT=55901
+function docker_sid_kde_full {
+  docker run -it --rm \
+             -p ${DOCKER_VNC_PORT}:5901 \
+             -v ${HOME}/Documents:/root/Documents \
+             -v ${HOME}/Media:/root/Media \
+             rgomes/sid-kde-full $*
+}
+
+function docker_sid_kdenlive {
   [[ ! -d $HOME/kdenlive ]] && mkdir -p $HOME/kdenlive
   docker run -it --rm \
-             -p ${VNC_PORT}:5901 \
+             -p ${DOCKER_VNC_PORT}:5901 \
              -v ${HOME}/kdenlive:/root/kdenlive \
              -v ${HOME}/Documents:/root/Documents \
              -v ${HOME}/Media:/root/Media \
-             rgomes/stretch-kdenlive $*
+             rgomes/sid-kdenlive $*
 }
