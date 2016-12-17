b4c85e35f3e4c4264c7e5469d7c2df96f94ac107
Richard Gomes <rgomes.info@gmail.com>
2016-12-17 19:23:16 +0000

    Introduce makepdf_frameless.sh

---
+++ bash_31makepdf_frameless.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# TODO: Frameless employ frames!
+#       I mean: the Frameless website employ frames, which confuses the convertion to PDF.
+
+
+function makepdf_frameless_files {
+cat <<EOD
+#olivierblanvillain.github.io/frameless/index.html
+olivierblanvillain.github.io/frameless/GettingStarted.html
+olivierblanvillain.github.io/frameless/TypedDatasetVsSparkDataset.html
+olivierblanvillain.github.io/frameless/TypedEncoder.html
+olivierblanvillain.github.io/frameless/Injection.html
+olivierblanvillain.github.io/frameless/Cats.html
+olivierblanvillain.github.io/frameless/TypedDataFrame.html
+#olivierblanvillain.github.io/dataframe/src/test/scala/BestNeighborhood.html
+#olivierblanvillain.github.io/dataframe/src/test/scala/JoinTests.html
+EOD
+}
+
+
+function makepdf_frameless {
+  local domain=olivierblanvillain.github.io
+  local path=frameless
+
+  local out="$HOME/Downloads/${path}.pdf"
+
+  httrack_fetch "$HOME/websites" "$domain" "$path" httrack "http://$domain/$path"
+
+  [[ -d "$HOME/websites/$domain/$path" ]] \
+  && pushd "$HOME/websites/$domain/$path" >> /dev/null \
+  &&   makepdf_frameless_files | pdf_converter_and_combiner $out \
+  && popd >> /dev/null \
+  && echo $out
+}
