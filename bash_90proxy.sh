c11cc20c608ef37c6d5fb90d7243b88762768196
Richard Gomes <rgomes.info@gmail.com>
2016-12-25 20:54:20 +0000

    Get ip for prosy server.

--- bash_90proxy.sh
+++ bash_90proxy.sh
@@ -6,14 +6,17 @@ function proxy_status {
 
 function proxy_on {
   if [ $(netstat -an | fgrep 3128 | wc -l) -gt 0 ] ;then
-    export JAVA_OPTS="${JAVA_OPTS} -Dhttp.proxyHost=localhost -Dhttp.proxyPort=3128"
-    export HTTP_PROXY=localhost:3128
-    export http_proxy=localhost:3128
-    export  FTP_PROXY=localhost:3128
-    export  ftp_proxy=localhost:3128
+    ip=$(netstat -an | fgrep ":3128 " | head -1 | sed -r 's/[ \t]+/ /g' | cut -d' ' -f5 | cut -d: -f1)
+    export JAVA_OPTS="${JAVA_OPTS} -Dhttp.proxyHost=${ip} -Dhttp.proxyPort=3128"
+    export HTTP_PROXY=http://${ip}:3128
+    export http_proxy=http://${ip}:3128
+    export  FTP_PROXY=http://${ip}:3128
+    export  ftp_proxy=http://${ip}:3128
   fi
 }
 
 function proxy_off {
   unset HTTP_PROXY http_proxy FTP_PROXY ftp_proxy
 }
+
+proxy_on
