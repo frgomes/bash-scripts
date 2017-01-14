257a672f89687718b9e2378c15e157867b1f4f37
Richard Gomes <rgomes.info@gmail.com>
2017-01-14 12:48:43 +0000

    Prefer downloadin Emacs sources from FTP instead of Git

--- install-emacs.sh
+++ install-emacs.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
+EMACS_VERSION=25.1
+
 sudo apt-get install autoconf automake libtool texinfo build-essential xorg-dev libgtk2.0-dev libjpeg-dev libncurses5-dev libdbus-1-dev libgif-dev libtiff-dev libm17n-dev libpng12-dev librsvg2-dev libotf-dev libgnutls28-dev libxml2-dev -y
 
-[[ ! -d ~/sources/git.sv.gnu.org ]] && mkdir -p ~/sources/git.sv.gnu.org
-cd /home/rgomes/sources/git.sv.gnu.org
+[[ ! -d ~/sources/ftp.gnu.org/emacs ]] && mkdir -p ~/sources/ftp.gnu.org/emacs
+cd /home/rgomes/sources/ftp.gnu.org/emacs
 
-if [ ! -d emacs ] ;then
-  git clone --depth 1 git://git.sv.gnu.org/emacs.git
-  cd emacs
-else
-  cd emacs
-  git pull
+if [ ! -d emacs-${EMACS_VERSION} ] ;then
+  curl https://ftp.gnu.org/pub/gnu/emacs/emacs-${EMACS_VERSION}.tar.xz | tar xpJf -
 fi
 
+cd emacs-${EMACS_VERSION}
+
 ./autogen.sh
-./configure --prefix=/opt/emacs25
+./configure --prefix=/opt/emacs/${EMACS_VERSION}
 make bootstrap
 make clean
 make
