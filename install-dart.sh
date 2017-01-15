3b75ecb3ff407083de28a9f54f0ff9fe81771390
Richard Gomes <rgomes.info@gmail.com>
2017-01-15 14:48:19 +0000

    Make sure version and architecture are explicit.

--- install-dart.sh
+++ install-dart.sh
@@ -13,4 +13,5 @@ mkdir -p /opt/developer && cd /opt/developer \
 && curl http://storage.googleapis.com/dart-archive/channels/stable/release/${DART_VERSION}/sdk/dartsdk-${DART_ARCH}-release.zip | bsdtar -xf - \
 && curl http://storage.googleapis.com/dart-archive/channels/stable/release/${DART_VERSION}/dartium/dartium-${DART_ARCH}-release.zip | bsdtar -xf - \
 && curl http://storage.googleapis.com/dart-archive/channels/stable/release/${DART_VERSION}/api-docs/dartdocs-gen-api.zip | bsdtar -xf - \
-&& mv gen-dartdocs dartdocs
+&& mv dart-sdk dart-sdk-${DART_VERSION}-${DART_ARCH} \
+&& mv gen-dartdocs dartdocs-${DART_VERSION}-${DART_ARCH}
