9ce05400bb5f7ca4e138f02255f71bf265cd44fe
Richard Gomes <rgomes.info@gmail.com>
2017-01-15 14:37:03 +0000

    Introduce install-node.sh

---
+++ install-node.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NODE_VERSION=${NODE_VERSION:=7.4.0}
+NODE_ARCH=${NODE_ARCH:=linux-x64}
+
+# make sure all necessary tools are installed
+if [ ! \( -e "$(which curl)" -a -e "$(which bsdtar)" \) ] ;then
+  echo apt-get install curl bsdtar -y
+  sudo apt-get install curl bsdtar -y
+fi
+
+mkdir -p /opt/developer && cd /opt/developer \
+&& curl http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-${NODE_ARCH}.tar.xz | bsdtar -xf -
