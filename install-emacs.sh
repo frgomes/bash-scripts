c47109cd9eb7db556860064cafbc5ae4ec53802f
Richard Gomes <rgomes.info@gmail.com>
2017-01-18 14:57:03 +0000

    Flexible way of obtaining defauls from either command line but also from environment variables.

--- install-emacs.sh
+++ install-emacs.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-EMACS_VERSION=25.1
+function install_emacs {
+  local version=$1
+  local version=${version:="$EMACS_VERSION"}
+  local version=${version:=24.4}
+  local major=$(echo $version | cut -d: -f1)
 
-sudo apt-get install autoconf automake libtool texinfo build-essential xorg-dev libgtk2.0-dev libjpeg-dev libncurses5-dev libdbus-1-dev libgif-dev libtiff-dev libm17n-dev libpng12-dev librsvg2-dev libotf-dev libgnutls28-dev libxml2-dev -y
+  sudo apt-get install autoconf automake libtool texinfo build-essential xorg-dev libgtk2.0-dev libjpeg-dev libncurses5-dev libdbus-1-dev libgif-dev libtiff-dev libm17n-dev libpng12-dev librsvg2-dev libotf-dev libgnutls28-dev libxml2-dev -y
 
-[[ ! -d ~/sources/ftp.gnu.org/emacs ]] && mkdir -p ~/sources/ftp.gnu.org/emacs
-cd /home/rgomes/sources/ftp.gnu.org/emacs
+  [[ ! -d ~/sources/ftp.gnu.org/emacs ]] && mkdir -p ~/sources/ftp.gnu.org/emacs
+  cd /home/rgomes/sources/ftp.gnu.org/emacs
 
-if [ ! -d emacs-${EMACS_VERSION} ] ;then
-  curl https://ftp.gnu.org/pub/gnu/emacs/emacs-${EMACS_VERSION}.tar.xz | tar xpJf -
-fi
+  if [ ! -d emacs-${version} ] ;then
+    curl https://ftp.gnu.org/pub/gnu/emacs/emacs-${version}.tar.xz | tar xpJf -
+  fi
 
-cd emacs-${EMACS_VERSION}
+  cd emacs-${version}
 
-./autogen.sh
-./configure --prefix=/opt/emacs/${EMACS_VERSION}
-make bootstrap
-make clean
-make
-sudo make install
+  ./autogen.sh
+  ./configure --prefix=/opt/emacs/${version}
+  make bootstrap
+  make clean
+  make
+  sudo make install
 
-sudo mkdir -p /opt/bin
-sudo ln -s /opt/emacs/${EMACS_VERSION}/bin/emacs /opt/bin/emacs
+  sudo mkdir -p /opt/bin
+  sudo ln -s /opt/emacs/${version}/bin/emacs /opt/bin/emacs${major}
+}
+
+# install_emacs
