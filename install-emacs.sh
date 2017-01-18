1c56a8621b60972595cf876904cd8445c4d43486
Richard Gomes <rgomes.info@gmail.com>
2017-01-18 15:24:34 +0000

    typo

--- install-emacs.sh
+++ install-emacs.sh
@@ -4,7 +4,7 @@ function install_emacs {
   local version=$1
   local version=${version:="$EMACS_VERSION"}
   local version=${version:=24.4}
-  local major=$(echo $version | cut -d: -f1)
+  local major=$(echo $version | cut -d. -f1)
 
   sudo apt-get install autoconf automake libtool texinfo build-essential xorg-dev libgtk2.0-dev libjpeg-dev libncurses5-dev libdbus-1-dev libgif-dev libtiff-dev libm17n-dev libpng12-dev librsvg2-dev libotf-dev libgnutls28-dev libxml2-dev -y
 
