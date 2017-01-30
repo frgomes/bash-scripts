def1a78f737625bbc38ec893dffcaa31903c8741
Richard Gomes <rgomes.info@gmail.com>
2017-01-30 20:44:42 +0000

    Upgrade typescript compiler.

--- install-node.sh
+++ install-node.sh
@@ -12,4 +12,5 @@ fi
 mkdir -p /opt/developer && cd /opt/developer \
 && curl http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-${NODE_ARCH}.tar.xz | bsdtar -xf - \
 && /opt/developer/node-v${NODE_VERSION}-${NODE_ARCH}/bin/npm install -g --save yarn \
-&& /opt/developer/node-v${NODE_VERSION}-${NODE_ARCH}/bin/npm install -g --save gulp@3.9.0 typescript@2.0.3 typings@2.1.0 tslint@4.4.2 angular-cli
+&& /opt/developer/node-v${NODE_VERSION}-${NODE_ARCH}/bin/npm install -g --save typescript angular-cli \
+&& /opt/developer/node-v${NODE_VERSION}-${NODE_ARCH}/bin/npm install -g --save gulp@3.9.0 typings@2.1.0 tslint@4.4.2
