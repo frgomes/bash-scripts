e9f39d1e27a3987b1a42a6af7471f145c78298fc
Richard Gomes <rgomes.info@gmail.com>
2017-01-30 04:40:06 +0000

    Gulp needs to be installed via npm.

--- install-node.sh
+++ install-node.sh
@@ -12,7 +12,5 @@ fi
 mkdir -p /opt/developer && cd /opt/developer \
 && curl http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-${NODE_ARCH}.tar.xz | bsdtar -xf - \
 && /opt/developer/node-v${NODE_VERSION}-${NODE_ARCH}/bin/npm install yarn -g --save \
-&& /opt/developer/node-v${NODE_VERSION}-${NODE_ARCH}/bin/yarn global add typescript typings \
-&& /opt/developer/node-v${NODE_VERSION}-${NODE_ARCH}/bin/yarn global add angular-cli \
-&& /opt/developer/node-v${NODE_VERSION}-${NODE_ARCH}/bin/yarn global add typescript@2.0.3 typings@2.1.0 tslint@4.4.2 \
-&& /opt/developer/node-v${NODE_VERSION}-${NODE_ARCH}/bin/yarn global add gulp@3.9.1
+&& /opt/developer/node-v${NODE_VERSION}-${NODE_ARCH}/bin/npm install gulp@3.9.0 -g --save \
+&& /opt/developer/node-v${NODE_VERSION}-${NODE_ARCH}/bin/yarn global add typescript@2.0.3 typings@2.1.0 tslint@4.4.2 angular-cli
