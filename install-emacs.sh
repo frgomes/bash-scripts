0ea66f97857a3976d8bbccc7b2957c4f72248029
Richard Gomes <rgomes.info@gmail.com>
2017-02-17 02:50:30 +0000

    Make installation independent of version of Debian distribution.

--- install-emacs.sh
+++ install-emacs.sh
@@ -3,10 +3,11 @@
 function install_emacs {
   local version=$1
   local version=${version:="$EMACS_VERSION"}
-  local version=${version:=24.4}
+  local version=${version:=25.1}
   local major=$(echo $version | cut -d. -f1)
 
-  sudo apt-get install autoconf automake libtool texinfo build-essential xorg-dev libgtk2.0-dev libjpeg-dev libncurses5-dev libdbus-1-dev libgif-dev libtiff-dev libm17n-dev libpng12-dev librsvg2-dev libotf-dev libgnutls28-dev libxml2-dev -y
+  sudo apt-get install curl -y
+  sudo apt-get build-dep emacs -y
 
   [[ ! -d ~/sources/ftp.gnu.org/emacs ]] && mkdir -p ~/sources/ftp.gnu.org/emacs
   cd /home/rgomes/sources/ftp.gnu.org/emacs
@@ -17,15 +18,17 @@ function install_emacs {
 
   cd emacs-${version}
 
+  [[ -e /opt/share/emacs/${version} ]] && rm /opt/share/emacs/${version}
+
   ./autogen.sh
-  ./configure --prefix=/opt/emacs/${version}
+  ./configure --prefix=/opt/share/emacs/${version}
   make bootstrap
   make clean
   make
   sudo make install
 
   sudo mkdir -p /opt/bin
-  sudo ln -s /opt/emacs/${version}/bin/emacs /opt/bin/emacs${major}
+  sudo ln -s /opt/share/emacs/${version}/bin/emacs /opt/bin/emacs${major}
 }
 
 install_emacs
