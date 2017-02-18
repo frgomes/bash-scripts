a7df86c0b790f37c21affe17ff910aa82aa52799
Richard Gomes <rgomes.info@gmail.com>
2017-02-18 07:14:18 +0000

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-NODE_VERSION=${NODE_VERSION:=6.4.0}
-NODE_ARCH=${NODE_ARCH:=linux-x64}
 
-# make sure all necessary tools are installed
-if [ ! \( -e "$(which curl)" -a -e "$(which bsdtar)" \) ] ;then
-  echo apt-get install curl bsdtar -y
-  sudo apt-get install curl bsdtar -y
-fi
+function install_node {
 
-mkdir -p /opt/developer && cd /opt/developer \
-&& curl http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-${NODE_ARCH}.tar.xz | bsdtar -xf -
+  local version=$1
+  local version=${version:="$NODE_VERSION"}
+  local version=${version:=7.5.0}
+
+  local arch=$2
+  local arch=${arch:="$NODE_ARCH"}
+  local arch=${arch:=linux-x64}
+
+  # make sure all necessary tools are installed
+  if [ ! \( -e "$(which curl)" -a -e "$(which bsdtar)" \) ] ;then
+    echo apt-get install curl bsdtar -y
+    sudo apt-get install curl bsdtar -y
+  fi
+
+  mkdir -p /opt/developer && cd /opt/developer \
+  && curl http://nodejs.org/dist/v${version}/node-v${version}-${arch}.tar.xz | bsdtar -xf -
+}
+
+
+install_node $*
