f4953549bbccdffbf8581069b33066a261b63b0f
Richard Gomes <rgomes.info@gmail.com>
2017-03-15 10:14:25 +0000

    Github utilities

---
+++ bash_22github.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##
+ # Lists all repositories of an organization in Github.
+ # BEWARE of ampersands!!! If your URL contain ampersands, make sure bash does not interpret it!!!
+ ##
+function github_query_org_repositories {
+  local org=${1:-}
+  local token=${2:-}
+  local tmp=$( tempfile )
+
+  if [ -z "$token" ] ;then
+    auth=""
+  else
+    auth="-u ${token}:x-oauth-basic"
+  fi
+
+  local n=1
+  while true ;do
+    [[ -f $tmp ]] && rm $tmp
+    ## curl -vs -u ${token}:x-oauth-basic https://api.github.com/orgs/${org}/repos?page=${n} 2>/dev/null | \
+    curl -vs ${auth} https://api.github.com/orgs/${org}/repos?page=${n} 2>/dev/null | \
+      fgrep ssh_url | \
+        sed -r 's/[ \t]+//g' | cut -d':' -f2- | tr -d '",' > $tmp
+    [[ ! -s $tmp ]] && break
+    cat ${tmp}
+    (( n++ ))
+  done
+}
+
+function github_org_repositories {
+  github_query_org_repositories $* | sort | uniq
+}
