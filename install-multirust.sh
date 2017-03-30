6be92dcdb6438409148eec5d13d460736f92620b
Richard Gomes <rgomes.info@gmail.com>
2017-03-30 21:38:48 +0100

    Retire install-multirust.sh

--- install-multirust.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -sSf https://static.rust-lang.org/rustup.sh | sh -s -- --uninstall
-curl -sf https://raw.githubusercontent.com/brson/multirust/master/blastoff.sh | sh
-multirust show-default
