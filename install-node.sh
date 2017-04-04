faa37f1ab97c48808a7d74089f2f7b2e3a07573e
Richard Gomes <rgomes.info@gmail.com>
2017-04-04 23:30:24 +0100

    Update install-node.sh and install-typescript.sh

--- install-node.sh
+++ install-node.sh
@@ -1,21 +1,29 @@
-#!/bin/bash
+#!/bin/bash -x
 
 
 function install_node {
   local version=${1:-"$NODE_VERSION"}
-  local version=${version:-"7.5.0"}
+  local version=${version:-"7.8.0"}
 
   local arch=${2:-"$NODE_ARCH"}
   local arch=${arch:-"linux-x64"}
 
   # make sure all necessary tools are installed
-  if [ ! \( -e "$(which curl)" -a -e "$(which bsdtar)" \) ] ;then
-    echo apt-get install curl bsdtar -y
-    sudo apt-get install curl bsdtar -y
+  if [ ! \( -e "$(which wget)" -a -e "$(which bsdtar)" \) ] ;then
+    echo apt-get install wget bsdtar -y
+    sudo apt-get install wget bsdtar -y
   fi
 
-  mkdir -p /opt/developer && cd /opt/developer \
-  && curl http://nodejs.org/dist/v${version}/node-v${version}-${arch}.tar.xz | bsdtar -xf -
+  [[ ! -d ~/Downloads ]] && mkdir -p ~/Downloads
+  pushd ~/Downloads
+  [[ ! -f node-v${version}-${arch}.tar.xz ]] \
+    && wget http://nodejs.org/dist/v${version}/node-v${version}-${arch}.tar.xz
+  popd
+  
+  [[ ! -d /opt/developer ]] && mkdir -p /opt/developer
+  pushd /opt/developer \
+    && bsdtar -xf ~/Downloads/node-v${version}-${arch}.tar.xz
+  popd
 }
 
 
--- install-typescript.sh
+++ install-typescript.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # make sure all necessary tools are installed
 if [ -e "$(which npm)" ] ;then
-  npm install -g --save yarn && yarn global add typescript ntypescript typings@2.1.0 tslint@4.4.2
+  npm install -g --save yarn && yarn global add typescript ntypescript typings tslint angular/cli
+  npm ls -g --depth=0
 else
-  echo "Please run: install-node.sh
+  echo "Please run: install-node.sh"
 fi
