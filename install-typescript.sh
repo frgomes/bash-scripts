7568c41a2fdfb4befa5f68ec414ba15ad5dcc173
Richard Gomes <rgomes.info@gmail.com>
2017-05-20 23:06:26 +0100

    Remove install-typescript since application builders can do that.

--- install-typescript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-# make sure all necessary tools are installed
-if [ -e "$(which npm)" ] ;then
-  npm install -g --save yarn && yarn global add typescript ntypescript typings tslint angular/cli
-  npm ls -g --depth=0
-else
-  echo "Please run: install-node.sh"
-fi
