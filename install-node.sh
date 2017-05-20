3ed5c3d9b819494e86553bab83abe13036f97537
Richard Gomes <rgomes.info@gmail.com>
2017-05-20 22:22:24 +0100

    Automagically install Angular CLI.

--- install-node.sh
+++ install-node.sh
@@ -8,26 +8,28 @@ function install_node {
   local arch=${2:-"$NODE_ARCH"}
   local arch=${arch:-"linux-x64"}
 
-  # make sure all necessary tools are installed
-  if [ ! \( -e "$(which wget)" -a -e "$(which bsdtar)" \) ] ;then
-    echo apt-get install wget bsdtar -y
-    sudo apt-get install wget bsdtar -y
-  fi
-
   [[ ! -d ~/Downloads ]] && mkdir -p ~/Downloads
   pushd ~/Downloads
   [[ ! -f node-v${version}-${arch}.tar.xz ]] \
     && wget http://nodejs.org/dist/v${version}/node-v${version}-${arch}.tar.xz
   popd
   
+  # make sure all necessary tools are installed
+  if [ ! \( -e "$(which wget)" -a -e "$(which bsdtar)" \) ] ;then
+    echo apt-get install wget bsdtar -y
+    sudo apt-get install wget bsdtar -y
+  fi
+
   [[ ! -d /opt/developer ]] && mkdir -p /opt/developer
   pushd /opt/developer \
-    && bsdtar -xf ~/Downloads/node-v${version}-${arch}.tar.xz \
-    && /opt/developer/node-v${version}-${arch}/bin/npm install npm@latest -g \
-    && /opt/developer/node-v${version}-${arch}/bin/node -v \
-    && /opt/developer/node-v${version}-${arch}/bin/npm -v
-  popd
+    && bsdtar -xf ~/Downloads/node-v${version}-${arch}.tar.xz
+
+  echo /opt/developer/node-v${version}-${arch}
 }
 
 
-install_node $*
+export NODE_HOME=$(install_node $*) \
+  && export PATH=$PATH:${NODE_HOME}/bin \
+    && npm install npm@latest -g \
+    && npm install -g @angular/cli \
+    && npm ls -g --depth=0
