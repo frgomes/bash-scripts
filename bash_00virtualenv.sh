072d7d26f7794e6ff1c2f2f01b948d60a38c5c78
Richard Gomes <rgomes.info@gmail.com>
2017-06-02 22:42:45 +0100

    Fix typos

--- bash_00virtualenv.sh
+++ bash_00virtualenv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ##################################################################################
-# This script allows managemente of multiple virtual environments.               # 
+# This script allows management of multiple virtual environments.                #
 # There are two ways of doing this:                                              #
-#  1. employ Python virtualenv and virtualenvwrapper;                            #
+#  1. employ Python virtualenv and virtualenvwrapper; (HIGHLY PREFERABLE)        #
 #  2. employ simplistic shell scripts, which are independent of Python.          #
 ##################################################################################
 
@@ -13,14 +13,14 @@ if [[ -f /usr/share/virtualenvwrapper/virtualenvwrapper.sh ]] ;then
   [[ ! -d ${WORKON_HOME} ]] && mkdir -p ${WORKON_HOME}
   source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 else
-  ##################################################################################
-  #                                                                                #
-  # This is a very simple replacement for Debian package python-virtualenv.        #
-  #                                                                                #
-  # The aim is avoid the long list of transient dependencies pulled out, including #
-  # development tools, which are not desired on production servers.                #
-  #                                                                                #
-  ##################################################################################
+  ###################################################################################
+  #                                                                                 #
+  # This is a very simple 'replacement' for Debian package python-virtualenv.       #
+  #                                                                                 #
+  # The aim is avoid the long list of transitive dependencies pulled out, including #
+  # development tools, which are not desired in production servers.                 #
+  #                                                                                 #
+  ###################################################################################
   function workon() {
       if [ ! -z "$1" -a -d ${WORKON_HOME}/"$1" ] ;then
           for script in activate postactivate ;do
