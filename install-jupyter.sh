d9878fce3489f40381f92dc74b24b954c28dbc96
Richard Gomes <rgomes.info@gmail.com>
2017-06-03 19:24:14 +0100

    Update install-jupyter.sh

--- install-jupyter.sh
+++ install-jupyter.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 function install_jupyter_core {
-  pip install jupyter --force
+  pip3 install --upgrade jupyter
 }
 
 function install_jupyter_coursier {
   [[ ! -d /opt/bin ]] && mkdir -p /opt/bin
-  cd /opt/bin
+  pushd /opt/bin
   [[ ! -e coursier ]] && \
     curl -L -o coursier https://git.io/vgvpD && \
       chmod +x coursier && pwd && ./coursier --help
+  popd
 }
 
 function install_jupyter_kernel_scala {
-  cd /tmp
-  curl -L -o jupyter-scala https://raw.githubusercontent.com/alexarchambault/jupyter-scala/master/jupyter-scala && chmod +x jupyter-scala && ./jupyter-scala
+  pushd /tmp
+  curl -L -o jupyter-scala https://raw.githubusercontent.com/alexarchambault/jupyter-scala/master/jupyter-scala \
+    && chmod +x jupyter-scala \
+      && ./jupyter-scala
+  popd
 }
 
-install_jupyter_core && install_jupyter_coursier && install_jupyter_kernel_scala
+install_jupyter_core \
+  && install_jupyter_coursier \
+    && install_jupyter_kernel_scala \
+      && hash -r; jupyter kernelspec list
