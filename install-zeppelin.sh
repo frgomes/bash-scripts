efbcb71003ece94f5941ce5aaa3ae177d89607e7
Richard Gomes <rgomes.info@gmail.com>
2017-06-04 15:27:53 +0100

    Review install-zeppelin.sh

--- install-zeppelin.sh
+++ install-zeppelin.sh
@@ -1,16 +1,54 @@
-#!/bin/bash
+#!/bin/bash -x
 
-mkdir ~/workspace
-pushd ~/workspace
 
-if [ -d zeppelin ] ;then
-  cd zeppelin
-  git pull
-else
-  git clone https://github.com/apache/zeppelin
-  cd zeppelin
-fi
+function download_zeppelin {
+  local apache=$HOME/sources/github.com/apache
+  local zeppelin=${apache}/zeppelin
+  local version=${1:-"$ZEPPELIN_VERSION"}
+  local version=${version:-"0.7.1"}
 
-mvn clean package -Pscala-2.11 -Pspark-1.6 -Phadoop-2.7 -Psparkr -Ppyspark -Pyarn -DskipTests
+  if [ -d ${zeppelin} ] ;then
+    pushd ${zeppelin}
+    git pull
+  else
+    mkdir -p ${apache} > /dev/null
+    pushd ${apache}
+    git clone http://github.com/apache/zeppelin
+    popd
+    pushd ${zeppelin}
+  fi
 
-popd
+  git checkout v${version}
+  git reset --hard
+
+  popd
+}
+
+
+function install_zeppelin {
+  local version=${1:-"$ZEPPELIN_VERSION"}
+  local version=${version:-"0.7.1"}
+
+  [[ ! -d ~/Downloads ]] && mkdir -p ~/Downloads
+  pushd ~/Downloads
+  [[ ! -f zeppelin-${version}-bin-all.tgz ]] \
+    && wget http://www-eu.apache.org/dist/zeppelin/zeppelin-${version}/zeppelin-${version}-bin-all.tgz
+  popd
+  
+  # make sure all necessary tools are installed
+  if [ ! \( -e "$(which wget)" -a -e "$(which bsdtar)" \) ] ;then
+    echo apt-get install wget bsdtar xz-utils -y
+    sudo apt-get install wget bsdtar xz-utils -y
+  fi
+
+  local tools=${TOOLS_HOME:=$HOME/tools}
+
+  [[ ! -d $tools ]] && mkdir -p $tools
+  pushd $tools \
+    && bsdtar -xf ~/Downloads/zeppelin-${version}-bin-all.tgz
+
+  echo $tools/zeppelin-${version}-bin-all
+}
+
+
+install_zeppelin
