a6ef8400a1200a4d624d4406035944daebb49306
Richard Gomes <rgomes.info@gmail.com>
2017-06-21 12:50:53 +0100

    Introduce github_user_repositories

--- bash_22github.sh
+++ bash_22github.sh
@@ -4,7 +4,7 @@
  # Lists all repositories of an organization in Github.
  # BEWARE of ampersands!!! If your URL contain ampersands, make sure bash does not interpret it!!!
  ##
-function github_query_org_repositories {
+function github_org_repositories {
   local org=${1:-}
   local token=${2:-}
   local tmp=$( tempfile )
@@ -25,9 +25,40 @@ function github_query_org_repositories {
     [[ ! -s $tmp ]] && break
     cat ${tmp}
     (( n++ ))
-  done
+  done \
+    | sort \
+      | uniq
+    
 }
 
-function github_org_repositories {
-  github_query_org_repositories $* | sort | uniq
+
+##
+ # Lists all repositories of an organization in Github.
+ # BEWARE of ampersands!!! If your URL contain ampersands, make sure bash does not interpret it!!!
+ ##
+function github_user_repositories {
+  local user=${1:-}
+  local token=${2:-}
+  local tmp=$( tempfile )
+
+  if [ -z "$token" ] ;then
+    auth=""
+  else
+    auth="-u ${token}:x-oauth-basic"
+  fi
+
+  local n=1
+  while true ;do
+    [[ -f $tmp ]] && rm $tmp
+    ## curl -vs -u ${token}:x-oauth-basic https://api.github.com/orgs/${org}/repos?page=${n} 2>/dev/null | \
+    curl -vs ${auth} https://api.github.com/users/${user}/repos?page=${n} 2>/dev/null | \
+      fgrep ssh_url | \
+        sed -r 's/[ \t]+//g' | cut -d':' -f2- | tr -d '",' > $tmp
+    [[ ! -s $tmp ]] && break
+    cat ${tmp}
+    (( n++ ))
+  done \
+    | sort \
+      | uniq
+    
 }
