6e99ee97207494e835167e88436a4ddcf73aafea
Richard Gomes <rgomes.info@gmail.com>
2017-07-02 18:25:24 +0100

    Remove alias to SBT.

--- bash_10aliases.sh
+++ bash_10aliases.sh
@@ -43,8 +43,6 @@ alias kdiff='kdiff3'
 alias vdiff='/opt/developer/idea/bin/idea.sh diff'
 
 
-alias sbt='sbt -mem 4096'
-
 ## finding contents in files easily
 
 alias sources='fgrep -v /target/ | fgrep -v /.hg/ | fgrep -v /.git/ | fgrep -v /.idea/'
