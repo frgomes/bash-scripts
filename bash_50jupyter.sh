4790fb5809f2485e1b013b273e250f8cb8761154
Richard Gomes <rgomes.info@gmail.com>
2017-10-18 01:17:07 +0100

    typo

--- bash_50jupyter.sh
+++ bash_50jupyter.sh
@@ -5,7 +5,7 @@ function jupyter_start {
   if [ ! -d $HOME/workspace/jupyter-notebooks ] ;then
     [[ ! -d $HOME/workspace ]] && mkdir -p $HOME/workspace
     pushd $HOME/workspace
-    git clohe http://github.com/frgomes/jupyter-notebooks
+    git clone http://github.com/frgomes/jupyter-notebooks
     popd
   fi
   pushd $HOME/workspace/jupyter-notebooks
