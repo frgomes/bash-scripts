0b14d58729b712785d41a3af6e3dc0e8934cd8d9
Richard Gomes <rgomes.info@gmail.com>
2017-10-19 19:46:03 +0100

    More monospaced fonts.

--- install-fonts-monospaced.sh
+++ install-fonts-monospaced.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install fonts-3270 fonts-fantasque-sans fonts-inconsolata -y
+sudo apt-get install fonts-3270 fonts-fantasque-sans fonts-inconsolata fonts-ricty-diminished fonts-hack-otf -y
