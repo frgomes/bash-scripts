a9518fcc8d6c3020d0dcca29408d8c835480fa05
Richard Gomes <rgomes.info@gmail.com>
2017-10-28 06:32:22 +0100

    Allow relocation of /var/lib/docker

--- install-docker.sh
+++ install-docker.sh
@@ -2,21 +2,26 @@
 
 # compiled from https://docs.docker.com/engine/installation/linux/debian/
 
-sudo apt-get update
-sudo apt-get dist-upgrade -y
-sudo apt-get install lsb-release apt-transport-https ca-certificates -y
+function install_docker_working_folder {
+  [[ ! -d /srv/lib/docker ]] && sudo mkdir -p /srv/lib/docker
+  [[ ! -d /var/lib/docker ]] && sudo ln -s /srv/lib/docker /var/lib/docker
+} 
 
-release=$(lsb_release -cs)
-sudo sh -c "echo deb https://apt.dockerproject.org/repo debian-${release} main > /etc/apt/sources.list.d/docker.list"
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+function install_docker {
+  sudo apt-get install lsb-release apt-transport-https dirmngr -y
 
-sudo apt-get update
-sudo apt-cache policy docker-engine
-sudo apt-get install docker-engine docker-compose -y
+  release=$(lsb_release -cs)
+  echo deb https://apt.dockerproject.org/repo debian-${release} main | sudo tee /etc/apt/sources.list.d/docker.list
+  sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-sudo service docker start
-sudo docker run hello-world
+  sudo apt-get update
+  sudo apt-cache policy docker-engine
+  sudo apt-get install docker-engine docker-compose -y
 
-sudo groupadd docker
-sudo gpasswd -a $USER docker
-sudo service docker restart
+  sudo groupadd docker
+  sudo gpasswd -a $USER docker
+  sudo service docker restart
+}
+
+
+install_docker_working_folder && install_docker
