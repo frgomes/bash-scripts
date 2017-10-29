a1170f30220addb35aa3296b6d6b4f9a8a0a14ad
Richard Gomes <rgomes.info@gmail.com>
2017-10-29 04:03:20 +0000

    Fix permissions.

--- install-libvirt.sh
+++ install-libvirt.sh
@@ -4,13 +4,13 @@
 function install_libvirt_working_folder {
   [[ ! -d /srv/lib/libvirt ]] && sudo mkdir -p /srv/lib/libvirt
   [[ ! -d /var/lib/libvirt ]] && sudo ln -s /srv/lib/libvirt /var/lib/libvirt
-  sudo chown -R libvirt-qemu:libvirt-qemu /srv/lib/libvirt
-  sudo chown    libvirt-qemu:libvirt-qemu /var/lib/libvirt
+  return 0
 }
 
 function install_libvirt {
   # install libvirt and vagrant
   sudo apt-get install libvirt-daemon libvirt-clients virt-top virtinst virt-manager vagrant vagrant-libvirt -y
+  sudo chown libvirt-qemu:libvirt-qemu /var/lib/libvirt /srv/lib/libvirt
 }
 
 function install_libvirt_user_enable {
@@ -20,8 +20,11 @@ function install_libvirt_user_enable {
 
   sudo usermod -a -G libvirt-qemu ${USER}
 
-  # amd or intel or maybe arm or whatever?
-  local cpu=amd
+  case $( cat /proc/cpuinfo | fgrep vendor_id | head -1 | sed -E 's/[ \t]+/ /g' | cut -d' ' -f3 ) in
+    AuthenticAMD) local cpu=amd   ;;
+    GenuineIntel) local cpu=intel ;;
+    *) local cpu=unknown ;;
+  esac
 
   # shake the tree
   sudo rmmod kvm_${cpu}
