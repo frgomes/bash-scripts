a1cac1c3e26775f82432155d42cf189025083509
Richard Gomes <rgomes.info@gmail.com>
2017-10-29 01:44:47 +0000

    Missing zstd compression.

--- install-general_purpose_tools.sh
+++ install-general_purpose_tools.sh
@@ -2,7 +2,7 @@
 
 
 function install_general_purpose_tools {
-  sudo apt-get install tar bsdtar bzip2 pbzip2 lbzip2 lzip plzip xz-utils pxz pigz zip unzip p7zip p7zip-rar -y
+  sudo apt-get install tar bsdtar bzip2 pbzip2 lbzip2 zstd lzip plzip xz-utils pxz pigz zip unzip p7zip p7zip-rar -y
   sudo apt-get install git gitk mercurial tortoisehg -y
   sudo apt-get install wget curl -y
   sudo apt-get install zile emacs25 -y
