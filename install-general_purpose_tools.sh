0d5a32e4683acf74093f617f3e4ce7ab4906af11
Richard Gomes <rgomes.info@gmail.com>
2017-11-02 03:00:58 +0000

    Remove creation of symlinks until tests are done properly.

--- install-general_purpose_tools.sh
+++ install-general_purpose_tools.sh
@@ -3,14 +3,15 @@
 
 function install_tools_compression {
   sudo apt-get install tar bsdtar bzip2 pbzip2 lbzip2 zstd lzip plzip xz-utils pxz pigz zip unzip p7zip p7zip-rar -y
-  [[ ! -e /usr/local/bin/bzip2   ]] && sudo ln -s /usr/bin/lbzip2 /usr/local/bin/bzip2
-  [[ ! -e /usr/local/bin/bunzip2 ]] && sudo ln -s /usr/bin/lbzip2 /usr/local/bin/bunzip2
-  [[ ! -e /usr/local/bin/bzcat   ]] && sudo ln -s /usr/bin/lbzip2 /usr/local/bin/bzcat
-  [[ ! -e /usr/local/bin/gzip    ]] && sudo ln -s /usr/bin/pigz   /usr/local/bin/gzip
-  [[ ! -e /usr/local/bin/gunzip  ]] && sudo ln -s /usr/bin/pigz   /usr/local/bin/gunzip
-  [[ ! -e /usr/local/bin/gzcat   ]] && sudo ln -s /usr/bin/pigz   /usr/local/bin/gzcat
-  [[ ! -e /usr/local/bin/lzip    ]] && sudo ln -s /usr/bin/plzip  /usr/local/bin/lzip
-  [[ ! -e /usr/local/bin/xz      ]] && sudo ln -s /usr/bin/pxz    /usr/local/bin/xz
+
+  #TODO: needs code review and tests!!!
+  #[[ ! -e /usr/local/bin/bzip2   ]] && sudo ln -s /usr/bin/lbzip2   /usr/local/bin/bzip2
+  #[[ ! -e /usr/local/bin/bunzip2 ]] && sudo ln -s /usr/bin/lbunzip2 /usr/local/bin/bunzip2
+  #[[ ! -e /usr/local/bin/bzcat   ]] && sudo ln -s /usr/bin/lbzcat   /usr/local/bin/bzcat
+  #[[ ! -e /usr/local/bin/gzip    ]] && sudo ln -s /usr/bin/pigz     /usr/local/bin/gzip
+  #[[ ! -e /usr/local/bin/gunzip  ]] && sudo ln -s /usr/bin/unpigz   /usr/local/bin/gunzip
+  #[[ ! -e /usr/local/bin/lzip    ]] && sudo ln -s /usr/bin/plzip    /usr/local/bin/lzip
+  #[[ ! -e /usr/local/bin/xz      ]] && sudo ln -s /usr/bin/pxz      /usr/local/bin/xz
 }
 
 function install_tools_scm {
