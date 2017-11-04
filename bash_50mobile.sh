18c0b7544db4c86a573844e16d3642c0f363414d
Richard Gomes <rgomes.info@gmail.com>
2017-11-04 05:06:55 +0000

    Code review.

--- bash_50mobile.sh
+++ bash_50mobile.sh
@@ -7,7 +7,9 @@ alias gmtool=~/tools/genymotion/gmtool
 
 
 function reactjs_update {
+  if [ ! -d node_modules ] ;then
     npm install && sbt clean update
+  fi
 }
 
 function reactjs_build {
@@ -15,6 +17,7 @@ function reactjs_build {
   local plat=${plat:="android"}
   local env=${2}
   local env=${env:="dev"}
+  reactjs_update
   sbt ~${plat}:${env}
 }
 
@@ -34,23 +37,28 @@ function reactjs_new {
   else
     local project=${1}
     local package=${2}
-    local package=${package:=io.mathminds.$project}
+    local package=${package:=$project}
 
-    pushd ~/workspace
-    if [ -d $project ] ;then
+    if [ -d ~/workspace/$project ] ;then
       echo ERROR: project already exists: $project
       return 1
     else
-      g8 file:///${HOME}/workspace/mobile.g8 --name=$project --package=$package
+      pushd ~/workspace
+      g8 file:///${HOME}/workspace/mobile.g8 --name=$project --package=$package -b expo-support
 ###   sbt new scalajs-react-interface/mobile.g8  --name=$project --package=$package -b drawer-navigation
-      pushd $project
-      react-native init $project
-      mv $project/android/ $project/ios/ .
-      rm -r -f $project
-      reactjs_update
-###   react-native link react-native-vector-icons
+      if [ ! -d $project ] ;then
+        echo ERROR: failed to create $project
+        return 1
+      else
+        pushd $project
+        react-native init $project
+        mv $project/android/ $project/ios/ .
+        rm -r -f $project
+        reactjs_update
+###     react-native link react-native-vector-icons
+        popd
+      fi
       popd
     fi
-    popd
   fi
 }
