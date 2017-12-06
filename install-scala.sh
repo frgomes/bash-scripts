5b8d2572dfd0d97c402db1736f43a8a431276792
Richard Gomes <rgomes.info@gmail.com>
2017-12-05 23:46:24 -0200

    Bump SBT version

--- install-scala.sh
+++ install-scala.sh
@@ -10,7 +10,7 @@ function install_scala_requirements {
 
 function install_scala_sbt {
   local version=${1:-"$SBT_VERSION"}
-  local version=${version:-"1.0.2"}
+  local version=${version:-"1.0.4"}
 
   [[ ! -d ~/Downloads ]] && mkdir -p ~/Downloads
   pushd ~/Downloads > /dev/null
