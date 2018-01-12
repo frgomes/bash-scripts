dec9c37f04bd25e420f00781cb2d90c180b0f4f1
Richard Gomes <rgomes.info@gmail.com>
2018-01-12 17:55:27 +0000

    Install rootkit detectors and scanssh tool.

--- install-security_hardening.sh
+++ install-security_hardening.sh
@@ -2,3 +2,6 @@
 
 sudo apt-get install lsb-release policycoreutils haveged network-manager-openvpn -y
 sudo restorecon -R -v ~/.cert
+
+# See: https://www.cyberciti.biz/faq/howto-check-linux-rootkist-with-detectors-software/
+sudo apt-get install chkrootkit rkhunter scanssh -y
