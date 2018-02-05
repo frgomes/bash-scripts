ee5aec49135e94d8e46e442e2835a64c946d0fdc
Richard Gomes <rgomes.info@gmail.com>
2018-02-05 10:06:27 +0000

    Review installation of JDK

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 source $HOME/bin/bash_20functions.sh
 
@@ -6,14 +6,13 @@ source $HOME/bin/bash_20functions.sh
 ## FIXME: There's hardcode in this function :-(
 ##
 function install_java {
-  ##FIXME: local version=${1:-"$JAVA_VERSION"}
-  ##FIXME: local version=${version:="1.8.0_151"}
-  local version="1.8.0_151"
+  local version=${1:-"$JAVA_VERSION"}
+  local version=${version:="1.8.0_161"}
 
   local tag=$( echo $version | sed 's/1.7.0_/7u/' | sed 's/1.8.0_/8u/' )
 
   ##FIXME: hardcode
-  local url=http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz
+  local url=http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.tar.gz
 
   # make sure all necessary tools are installed
   if [ ! \( -e "$(which wget)" \) -a \( -e "$(which bsdtar)" \) -a \( -e "$(which unzip)" \) -a \( -e "$(which httrack)" \) ] ;then
--- scripts/install-java_plugin.sh
+++
@@ -1,1 +0,0 @@
-sudo ln -s ${JAVA_HOME}/jre/lib/amd64/libnpjp2.so /usr/lib/mozilla/plugins/
