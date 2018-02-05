db32ca7f7304c2f57a32a5f234467d7137fe1b0a
Richard Gomes <rgomes.info@gmail.com>
2018-02-05 13:34:23 +0000

    Remove sudo from commands

--- postinstall-sysadmin.sh
+++ postinstall-sysadmin.sh
@@ -4,14 +4,14 @@ apt-get update -y
 apt-get dist-upgrade -y
 apt-get autoremove --purge -y
 
-apt-get install sudo -y
+apt-get install -y
 
 
 ##------------------------------------------
 
 
 function postinstall_remove_if_installed {
-  dpkg-query -W $* | while read pkg ;do sudo apt-get remove --purge -y $pkg ;done
+  dpkg-query -W $* | while read pkg ;do apt-get remove --purge -y $pkg ;done
 }
 
 
@@ -19,59 +19,59 @@ function postinstall_remove_if_installed {
 
 
 function postinstall_compression {
-  sudo apt-get install tar bsdtar bzip2 pbzip2 lbzip2 zstd lzip plzip xz-utils pxz pigz zip unzip p7zip p7zip-rar -y
+  apt-get install tar bsdtar bzip2 pbzip2 lbzip2 zstd lzip plzip xz-utils pxz pigz zip unzip p7zip p7zip-rar -y
 
   #TODO: needs code review and tests!!!
-  #[[ ! -e /usr/local/bin/bzip2   ]] && sudo ln -s /usr/bin/lbzip2   /usr/local/bin/bzip2
-  #[[ ! -e /usr/local/bin/bunzip2 ]] && sudo ln -s /usr/bin/lbunzip2 /usr/local/bin/bunzip2
-  #[[ ! -e /usr/local/bin/bzcat   ]] && sudo ln -s /usr/bin/lbzcat   /usr/local/bin/bzcat
-  #[[ ! -e /usr/local/bin/gzip    ]] && sudo ln -s /usr/bin/pigz     /usr/local/bin/gzip
-  #[[ ! -e /usr/local/bin/gunzip  ]] && sudo ln -s /usr/bin/unpigz   /usr/local/bin/gunzip
-  #[[ ! -e /usr/local/bin/lzip    ]] && sudo ln -s /usr/bin/plzip    /usr/local/bin/lzip
-  #[[ ! -e /usr/local/bin/xz      ]] && sudo ln -s /usr/bin/pxz      /usr/local/bin/xz
+  #[[ ! -e /usr/local/bin/bzip2   ]] && ln -s /usr/bin/lbzip2   /usr/local/bin/bzip2
+  #[[ ! -e /usr/local/bin/bunzip2 ]] && ln -s /usr/bin/lbunzip2 /usr/local/bin/bunzip2
+  #[[ ! -e /usr/local/bin/bzcat   ]] && ln -s /usr/bin/lbzcat   /usr/local/bin/bzcat
+  #[[ ! -e /usr/local/bin/gzip    ]] && ln -s /usr/bin/pigz     /usr/local/bin/gzip
+  #[[ ! -e /usr/local/bin/gunzip  ]] && ln -s /usr/bin/unpigz   /usr/local/bin/gunzip
+  #[[ ! -e /usr/local/bin/lzip    ]] && ln -s /usr/bin/plzip    /usr/local/bin/lzip
+  #[[ ! -e /usr/local/bin/xz      ]] && ln -s /usr/bin/pxz      /usr/local/bin/xz
 }
 
 function postinstall_scm {
-  sudo apt-get install git mercurial -y
+  apt-get install git mercurial -y
 }
 
 function postinstall_downloads {
-  sudo apt-get install wget curl -y
+  apt-get install wget curl -y
 }
 
 function postinstall_editors {
-  sudo apt-get install zile vim -y
+  apt-get install zile vim -y
 }
 
 function postinstall_apt {
-  sudo apt-get install apt-file apt-transport-https apt-utils -y
-  sudo apt-file update
+  apt-get install apt-file apt-transport-https apt-utils -y
+  apt-file update
 }
 
 function postinstall_security_hardening {
-  sudo apt-get install lsb-release -y
-  sudo apt-get install fail2ban -y
-  sudo apt-get install policycoreutils haveged network-manager-openvpn -y
-  sudo restorecon -R -v ~/.cert
+  apt-get install lsb-release -y
+  apt-get install fail2ban -y
+  apt-get install policycoreutils haveged network-manager-openvpn -y
+  restorecon -R -v ~/.cert
   # See: https://www.cyberciti.biz/faq/howto-check-linux-rootkist-with-detectors-software/
-  sudo apt-get install chkrootkit rkhunter scanssh -y
+  apt-get install chkrootkit rkhunter scanssh -y
 }
 
 function postinstall_networking {
-  sudo apt-get install dnsutils nmap -y
+  apt-get install dnsutils nmap -y
 }
 
 function postinstall_virtualenv {
-  sudo apt-get install virtualenvwrapper -y
+  apt-get install virtualenvwrapper -y
   source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 }
 
 function postinstall_source_code_utils {
-  sudo apt-get install less source-highlight -y
+  apt-get install less source-highlight -y
 }
 
 function postinstall_firefox {
-  [[ ! -d /root/Downloads ]] && sudo mkdir -p /root/Downloads
+  [[ ! -d /root/Downloads ]] && mkdir -p /root/Downloads
   local app=firefox
   local lang=$(echo $LANG | cut -d. -f1 | sed "s/_/-/")
   local hwarch=$(uname -m)
@@ -91,17 +91,17 @@ function postinstall_firefox {
     popd 2>&1 > /dev/null
   fi
   if [ -L /usr/local/bin/${app} ] ;then
-    sudo rm /usr/local/bin/${app}
+    rm /usr/local/bin/${app}
   fi
 
-  sudo ln -s /opt/${app}/${app} /usr/local/bin/${app}
+  ln -s /opt/${app}/${app} /usr/local/bin/${app}
   echo /usr/local/bin/${app}
 
   postinstall_remove_if_installed firefox-esr
 }
 
 function postinstall_thunderbird {
-  [[ ! -d /root/Downloads ]] && sudo mkdir -p /root/Downloads
+  [[ ! -d /root/Downloads ]] && mkdir -p /root/Downloads
   local app=thunderbird
   local lang=$(echo $LANG | cut -d. -f1 | sed "s/_/-/")
   local hwarch=$(uname -m)
@@ -121,10 +121,10 @@ function postinstall_thunderbird {
     popd 2>&1 > /dev/null
   fi
   if [ -L /usr/local/bin/${app} ] ;then
-    sudo rm /usr/local/bin/${app}
+    rm /usr/local/bin/${app}
   fi
 
-  sudo ln -s /opt/${app}/${app} /usr/local/bin/${app}
+  ln -s /opt/${app}/${app} /usr/local/bin/${app}
   echo /usr/local/bin/${app}
 
   postinstall_remove_if_installed thunderbird lightning
@@ -145,10 +145,10 @@ function postinstall_utilities_wp34s {
   fi
   
   if [ -L /usr/local/bin/WP-34s ] ;then
-    sudo rm /usr/local/bin/WP-34s
+    rm /usr/local/bin/WP-34s
   fi
       
-  sudo ln -s /opt/wp-34s/WP-34s /usr/local/bin
+  ln -s /opt/wp-34s/WP-34s /usr/local/bin
   echo /usr/local/bin/WP-34S
 }
 
@@ -161,13 +161,13 @@ function postinstall_monitoring {
 
 
 function postinstall_x11 {
-  sudo apt-get install xclip gitk tortoisehg zeal -y
-  sudo apt-get install chromium -y
-  sudo apt-get install emacs25 -y
+  apt-get install xclip gitk tortoisehg zeal -y
+  apt-get install chromium -y
+  apt-get install emacs25 -y
 }
 
 function postinstall_console {
-  sudo apt-get install emacs25-nox -y
+  apt-get install emacs25-nox -y
 }
 
 
