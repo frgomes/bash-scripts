2e52fc5c0849c600b1e64a5f3e5f6d6e07dbd30a
Richard Gomes <rgomes.info@gmail.com>
2018-02-09 21:07:11 +0000

    Get rid of instructions needing sudo privilege escalation.

--- bash_21dpkg.sh
+++ bash_21dpkg.sh
@@ -5,9 +5,3 @@ function dpkg_list() {
     apt list --installed 2> /dev/null | fgrep "$1"
   fi
 }
-
-function dpkg_purge() {
-  for pkg in `dpkg --list | grep -E '^rc' | awk '{ print $2 }' ` ;do
-    sudo dpkg --purge $pkg
-  done
-}
