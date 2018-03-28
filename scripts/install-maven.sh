9547a59bbfc1d713e9c4d7a932e58f1f59c90c52
Richard Gomes <rgomes.info@gmail.com>
2018-03-28 20:55:38 +0100

    install-maven.sh

---
+++ scripts/install-maven.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+function install_maven {
+  local version=${1:-"$M2_VERSION"}
+  local version=${version:-"3.5.3"}
+
+  [[ ! -d ~/Downloads ]] && mkdir -p ~/Downloads
+  pushd ~/Downloads
+  [[ ! -f apache-maven-${version}-bin.tar.gz ]] \
+    && wget http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-3/${version}/binaries/apache-maven-${version}-bin.tar.gz
+  popd
+  
+  local tools=${TOOLS_HOME:=$HOME/tools}
+
+  [[ ! -d $tools ]] && mkdir -p $tools
+  pushd $tools \
+    && bsdtar -xf ~/Downloads/apache-maven-${version}-bin.tar.gz
+
+  echo $tools/apache-maven-${version}
+}
+
+export M2_HOME=$(install_maven $*) && echo $M2_HOME \
+  && export PATH=$PATH:${M2_HOME}/bin
