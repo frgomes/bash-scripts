00745330c9cd3dbf842e6bb85eb18e4028f2bdb3
Richard Gomes <rgomes.info@gmail.com>
2018-04-05 15:50:03 +0100

    Bugfix caused by merge of projects debian-bin and debian-scripts.

--- bash_04virtualenv.sh
+++ bash_04virtualenv.sh
@@ -3,24 +3,11 @@
 
 # List profiles made available by debian-bin
 function virtualenv_profile_list {
-  if [ -d $HOME/bin/bashrc-virtualenvs ] ;then
-    pushd $HOME/bin/bashrc-virtualenvs > /dev/null
+  if [ -d $HOME/scripts/bashrc-virtualenvs ] ;then
+    pushd $HOME/scripts/bashrc-virtualenvs > /dev/null
     find . -type f -name postactivate
     popd > /dev/null
   fi
-}
-
-
-# Relink virtualenvs to corresponding profiles, if possible, if needed.
-function virtualenv_profile_relink_all {
-  local tstamp=$(date +%Y%m%d%H%M%S)
-  virtualenv_profile_list | while read source ;do
-    local target=$HOME/.virtualenvs/${source}
-    if [ -f ${target} ] ;then
-      mv ${target} ${target}.${tstamp}
-      ln -s $HOME/bin/bashrc-virtualenvs/${source} ${target}
-    fi
-  done
 }
 
 
@@ -28,11 +15,15 @@ function virtualenv_profile_relink_all {
 function virtualenv_profile_link_all {
   local tstamp=$(date +%Y%m%d%H%M%S)
   virtualenv_profile_list | while read source ;do
+    local envbin=$(dirname $source)
+    mkdir -p $HOME/.virtualenvs/${envbin} > /dev/null 2>&1
     local target=$HOME/.virtualenvs/${source}
-    if [ \( ! -L ${target} \) -a \( -f ${target} \) ] ;then
+    if [ -L ${target} ] ;then
+      rm ${target}
+    elif [ -f ${target} ] ;then
       mv ${target} ${target}.${tstamp}
-      ln -s $HOME/bin/bashrc-virtualenvs/${source} ${target}
     fi
+    ln -s $HOME/scripts/bashrc-virtualenvs/${source} ${target}
   done
 }
 
