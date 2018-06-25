e089c1f1287f1d5ae8efa20f7c9bb9842f66221f
Richard Gomes <rgomes.info@gmail.com>
2018-06-25 17:26:21 +0100

    install-groovy.sh

---
+++ scripts/user-install/install-groovy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function install_groovy {
+  local version=${1:-"$GROOVY_VERSION"}
+  local version=${version:-"2.5.0"}
+
+  local major=$( echo ${version} | cut -d. -f 1-2 )
+
+
+https://dl.bintray.com/groovy/maven/apache-groovy-sdk-2.5.0.zip
+
+
+  [[ ! -d ~/Downloads ]] && mkdir -p ~/Downloads
+  pushd ~/Downloads > /dev/null
+  [[ ! -f apache-groovy-sdk-${version}.zip ]] && wget https://dl.bintray.com/groovy/maven/apache-groovy-sdk-${version}.zip
+  popd > /dev/null
+
+  local tools=${TOOLS_HOME:=$HOME/tools}
+
+  [[ ! -d ${tools} ]] && mkdir -p ${tools}
+  
+  if [ ! -d ${tools}/groovy-${version} ] ;then
+    pushd ${tools} > /dev/null
+    unzip ~/Downloads/apache-groovy-sdk-${version}.zip
+    popd > /dev/null
+  fi
+
+  echo ${tools}/groovy-${version}
+}
+
+
+install_groovy $*
