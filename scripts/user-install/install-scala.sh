b48bb24bf7c88090965a81a81df9ed1576ae2d3c
Richard Gomes <rgomes.info@gmail.com>
2018-06-27 19:52:56 +0100

    More robust code.

--- scripts/user-install/install-scala.sh
+++ scripts/user-install/install-scala.sh
@@ -86,7 +86,10 @@ function install_scala {
 [[ ! -d ~/bin ]] && mkdir -p ~/bin
 
 ( install_scala_sbt && install_scala_sbt_ensime && install_scala $* ) | while read dir ;do
-  name=$(basename $dir | cut -d- -f1)
-  [[ -e ~/bin/${name} ]] && rm ~/bin/${name}
-  ln -s ${dir}/bin/${name} ~/bin/${name}
+  echo ${dir}
+  name=$(basename ${dir} | cut -d'-' -f1)
+  if [ ! -z ${name} ] ;then
+    [[ -f ~/bin/${name} ]] && rm ~/bin/${name}
+    ln -s ${dir}/bin/${name} ~/bin/${name}
+  fi
 done
