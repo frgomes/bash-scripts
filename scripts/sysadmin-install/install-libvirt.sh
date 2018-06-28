2bbdcd91ce36e264466757220a4c437afc7f6aac
Richard Gomes <rgomes.info@gmail.com>
2018-06-28 14:06:21 +0100

    Move scripts to a common place for sysadmin scripts.

--- scripts/sysadmin-install/install-libvirt.sh
+++ scripts/sysadmin-install/install-libvirt.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 
 
-function postinstall_sysadmin_libvirt {
+function install_libvirt {
   [[ ! -d /srv/lib/libvirt ]] && mkdir -p /srv/lib/libvirt
   [[ ! -d /var/lib/libvirt ]] && ln -s /srv/lib/libvirt /var/lib/libvirt
   apt install libvirt-daemon libvirt-daemon-system libvirt-clients virt-top xmlstarlet uuid-runtime -y
 }
 
-function postinstall_sysadmin_libvirt_modprobe {
+function install_libvirt_modprobe {
   case $( cat /proc/cpuinfo | fgrep vendor_id | head -1 | sed -E 's/[ \t]+/ /g' | cut -d' ' -f3 ) in
     AuthenticAMD) local cpu=amd   ;;
     GenuineIntel) local cpu=intel ;;
@@ -21,4 +21,4 @@ function postinstall_sysadmin_libvirt_modprobe {
   modprobe kvm_${cpu}
 }
 
-postinstall_sysadmin_libvirt && postinstall_sysadmin_libvirt_modprobe
+install_libvirt && install_libvirt_modprobe
