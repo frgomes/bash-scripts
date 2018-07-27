9f285acd15a8bfa73b225c892470afd37d3f0414
Richard Gomes <rgomes@mathminds.io>
2018-07-27 08:32:15 +0100

    Move install-jitsi_desktop.sh

--- sysadmin-install/install-jitsi_desktop.sh
+++ sysadmin-install/install-jitsi_desktop.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
 
-
 wget -qO - https://download.jitsi.org/jitsi-key.gpg.key | sudo apt-key add -
 echo 'deb https://download.jitsi.org stable/' | sudo tee /etc/apt/sources.list.d/jitsi-stable.list
 sudo apt update -y
