9399fe8551fa796548ff8cbe4e1b930e5e3464e8
Richard Gomes <rgomes@mathminds.io>
2018-07-27 08:31:48 +0100

    Bump Swagger codegen to 3.x

--- bash_50swagger.sh
+++ bash_50swagger.sh
@@ -8,10 +8,9 @@ function swagger_editor {
 
 
 function swagger_cli_download {
-  local r=snapshots
-  local v=3.0.0-SNAPSHOT
-  local name=swagger-codegen-cli-3.0.0-20180630.155857-85-javadoc.jar
-  local url=https://oss.sonatype.org/content/repositories/${r}/io/swagger/swagger-codegen-cli/${v}/${name}
+  local v=3.1.0
+  local name=swagger-codegen-cli-3.0.0-20180630.155857-85.jar
+  local url=http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/${v}/openapi-generator-cli-${v}.jar
   local file=~/Downloads/$name
   if [ ! -f $file ] ;then
     mkdir -p ~/Downloads > /dev/null
@@ -22,16 +21,20 @@ function swagger_cli_download {
   echo $file
 }
 
+function swagger_codegen {
+  local jar=$(swagger_cli_download)
+  java -jar $jar $*
+}
 
 function swagger_finch {
   local jar=$(swagger_cli_download)
   if [ $# != 2 ] ;then
     echo 'swagger_finch <input URL> <output DIR>'
+    java -jar $jar -h generate
   else
     java -jar $jar generate -l finch -i $1 -o $2
   fi
 }
-
 
 function swagger_angular {
   local jar=$(swagger_cli_download)
