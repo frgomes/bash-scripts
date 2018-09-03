93cfd77ca5ba53183bff9d957628c0789eb09efa
Richard Gomes <rgomes.info@gmail.com>
2018-09-03 18:08:28 +0100

    Update jupyter-scala kernel.

--- user-install/install-jupyter.sh
+++ user-install/install-jupyter.sh
@@ -5,14 +5,13 @@ function install_jupyter_core {
 }
 
 function install_jupyter_coursier {
-  pushd /usr/local/bin
-  [[ ! -e coursier ]] && \
-    sudo curl -L -o coursier https://git.io/vgvpD && \
-      sudo chmod 755 coursier && pwd && ./coursier --help
-  popd
+  mkdir -p $HOME/bin
+  [[ ! -e $HOME/bin/coursier ]] && \
+    curl -L -o $HOME/bin/coursier https://git.io/vgvpD && \
+      chmod 755 $HOME/bin/coursier && pwd && $HOME/bin/coursier --help
 }
 
-function install_jupyter_kernel_scala {
+function install_jupyter_kernel_scala_211 {
   pushd /tmp
   curl -L -o jupyter-scala https://raw.githubusercontent.com/alexarchambault/jupyter-scala/master/jupyter-scala \
     && chmod +x jupyter-scala \
@@ -20,7 +19,17 @@ function install_jupyter_kernel_scala {
   popd
 }
 
+function install_jupyter_kernel_scala_212 {
+  local SCALA_VERSION=2.12.6
+  local ALMOND_VERSION=0.1.5
+  $HOME/bin/coursier bootstrap \
+     --standalone --force \
+     -i user -I user:sh.almond:scala-kernel-api_${SCALA_VERSION}:${ALMOND_VERSION} \
+     sh.almond:scala-kernel_${SCALA_VERSION}:${ALMOND_VERSION} \
+     -o $HOME/bin/almond
+}
+
 install_jupyter_core \
   && install_jupyter_coursier \
-    && install_jupyter_kernel_scala \
+    && install_jupyter_kernel_scala_212 \
       && hash -r; jupyter kernelspec list
