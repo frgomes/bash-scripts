e8d7642903ec76dccc65859a2d1bc9576161c6a5
Richard Gomes <rgomes@mathminds.io>
2018-09-21 10:35:01 +0100

    Add alias for listing listening ports.

--- bash_10aliases.sh
+++ bash_10aliases.sh
@@ -41,9 +41,14 @@ alias cdiff='diff -Naur'
 alias sdiff='diff -Npry'
 alias kdiff='kdiff3'
 alias vdiff='${TOOLS_HOME}/idea/bin/idea.sh diff'
+
+# network utilities
+alias listening='netstat -an | fgrep LISTEN | fgrep -v LISTENING'
+
+# docker utilities
 alias beakerx='docker run -p 8888:8888 beakerx/beakerx $*'
 
-# caopy/paste using the clipboard
+# copy/paste using the clipboard
 alias ctrlc='xclip -i -selection clipboard'
 alias ctrlv='xclip -o -selection clipboard'
 
