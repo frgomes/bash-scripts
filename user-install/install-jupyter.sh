4ab77773fcfbe66f2db48ee366be7e2d48df69ec
Richard Gomes <rgomes@mathminds.io>
2018-10-04 01:43:51 +0100

    Upgrade to Almond, a new Scala kernel for Jupyter.

--- user-install/install-jupyter.sh
+++ user-install/install-jupyter.sh
@@ -1,38 +1,31 @@
 #!/bin/bash
 
 function install_jupyter_core {
+  pip3 install --upgrade pip
   pip3 install --upgrade jupyter
 }
 
 function install_jupyter_coursier {
-  mkdir -p $HOME/bin
   if [ ! -e $HOME/bin/coursier ] ;then
-    curl -L -o $HOME/bin/coursier https://git.io/vgvpD && \
-      chmod 755 $HOME/bin/coursier && pwd && $HOME/bin/coursier --help
+    curl -L -o $HOME/bin/coursier https://git.io/coursier \
+      && chmod +x $HOME/bin/coursier \
+        && $HOME/bin/coursier --help
   fi
 }
 
-function install_jupyter_kernel_scala {
-  pushd /tmp
-  curl -L -o jupyter-scala https://raw.githubusercontent.com/alexarchambault/jupyter-scala/master/jupyter-scala \
-    && chmod +x jupyter-scala \
-      && ./jupyter-scala --force
-  popd
+function install_jupyter_kernel_scala_2_12 {
+  local SCALA_VERSION=2.12.7
+  local ALMOND_VERSION=0.1.9
+
+  coursier bootstrap \
+    -i user -I user:sh.almond:scala-kernel-api_$SCALA_VERSION:$ALMOND_VERSION \
+    sh.almond:scala-kernel_$SCALA_VERSION:$ALMOND_VERSION \
+    -f -o ${HOME}/bin/almond \
+      && ${HOME}/bin/almond --install --force
 }
 
-## This is a tentative only at this point
-function install_jupyter_kernel_scala_TRIAL_212 {
-  local SCALA_VERSION=2.12.6
-  local ALMOND_VERSION=0.1.5
-  $HOME/bin/coursier bootstrap -f \
-     --standalone \
-     -i user -I user:sh.almond:scala-kernel-api_${SCALA_VERSION}:${ALMOND_VERSION} \
-     sh.almond:scala-kernel_${SCALA_VERSION}:${ALMOND_VERSION} \
-     -o $HOME/bin/almond && \
-    $HOME/bin/almond --install
-}
-
+mkdir -p ${HOME}/bin
 install_jupyter_core \
   && install_jupyter_coursier \
-    && install_jupyter_kernel_scala \
+    && install_jupyter_kernel_scala_2_12 \
       && hash -r; jupyter kernelspec list
