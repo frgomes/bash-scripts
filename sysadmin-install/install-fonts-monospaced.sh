374c86a2dfcc2fe8e3accd078d42089028227450
Richard Gomes <rgomes.info@gmail.com>
2018-10-17 10:51:25 +0100

    Install more fixed width and monospaced fonts

---
+++ sysadmin-install/install-fonts-monospaced.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt install -y fonts-3270 fonts-fantasque-sans fonts-inconsolata fonts-ricty-diminished fonts-hack-otf \
+                    xfonts-terminus xfonts-traditional fonts-firacode fonts-jura fonts-noto-mono
