92cab4d2c63323c557e5e604cd50015aa2476639
Richard Gomes <rgomes.info@gmail.com>
2019-02-01 13:15:03 +0000

    Install additional tools to Go development environment.

--- user-install/install-go.sh
+++ user-install/install-go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 
-function install_go {
+function install_go_binaries {
   local version=${1:-"$GO_VERSION"}
   local version=${version:-"1.11.5"}
 
@@ -22,6 +22,35 @@ function install_go {
     && tar -xf ~/Downloads/${archive}
 
   echo $tools/go${version}.${arch}
+}
+
+function install_go_tools {
+  local tools=${TOOLS_HOME:-${HOME}/tools}
+  local gohome=${GO_HOME:-${tools}/go}
+  local gopath=${GOPATH:-${HOME}/go}
+
+  local PATH=${PATH}:${gohome}
+  local GOPATH=${gopath}
+
+  pushd $gopath
+
+  ## GoMetaLinter: https://github.com/alecthomas/gometalinter
+  curl -s -L https://git.io/vp6lP | sh
+
+  ## TODO: verify if these tools are desirable
+  #golang.org/x/tools/cmd/goimports
+  #github.com/qiniu/checkstyle/gocheckstyle
+  #github.com/KyleBanks/depth/cmd/depth
+  #github.com/bradleyfalzon/apicompat/cmd/apicompat
+  for app in github.com/nsf/gocode ;do
+    go get -u ${app}
+  done
+
+  popd
+}
+
+function install_go {
+  install_go_binaries && install_go_tools
 }
 
 
