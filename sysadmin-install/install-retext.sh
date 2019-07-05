0525b9ffbfc8917cda777a897b5891261d0e32de
Richard Gomes <rgomes.info@gmail.com>
2019-07-05 21:31:30 +0200

    install-retext.sh

---
+++ sysadmin-install/install-retext.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+function install_retext {
+  sudo apt install -y formiko retext libjs-mathjax python3-regex fonts-mathjax-extras fonts-stix libjs-mathjax-doc fonts-roboto
+}
+
+
+if [ $_ != $0 ] ;then
+  # echo "Script is being sourced"
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  # echo $dir
+  # echo $self
+  fgrep "function " $self | cut -d' ' -f2 | head -n -2
+else
+  # echo "Script is a subshell"
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  # echo $dir
+  # echo $self
+  cmd=$(fgrep "function " $self | cut -d' ' -f2 | head -n -2 | tail -1)
+  # echo $cmd
+  $cmd $*
+fi
