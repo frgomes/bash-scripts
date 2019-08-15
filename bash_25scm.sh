89f211439d880343b143761fae270a3761434dd4
Richard Gomes <rgomes.info@gmail.com>
2019-08-15 19:24:55 +0200

    add function command gitlink

--- bash_25scm.sh
+++ bash_25scm.sh
@@ -123,6 +123,29 @@ function git_http_to_ssh {
 }
 
 
+function gitlink {
+    local name=${1}
+    local name=${name:=origin}
+    local url=$( git remote get-url $name )
+    [[ "$url" =~ (.+)@(.+):(.+)/(.+)(\.git)? ]]
+    local user="${BASH_REMATCH[1]}"
+    if [ "$user" == "git" ] ;then
+      local provider="${BASH_REMATCH[2]}"
+      local team="${BASH_REMATCH[3]}"
+      local prj="${BASH_REMATCH[4]}"
+      local ext="${BASH_REMATCH[5]}"
+      # workaround https://bitbucket.org/site/master/issues/5154/someone-has-already-registered-that-ssh
+      company=$(fgrep Host ~/.ssh/config | fgrep -v Hostname | cut -d' ' -f2)
+      if [ X"${company}" != "X" -a X"${provider}" == "Xbitbucket.org" -a -f "~/.ssh/id_rsa_${team}" ] ;then
+        provider="${company}"
+      fi
+      firefox "https://${provider}/${team}/${prj}${ext}" &
+    else
+      firefox "${url}" &
+    fi
+}
+
+
 function git_ssh_to_http {
     local name=${1}
     local name=${name:=origin}
