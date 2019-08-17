25f54007e45fb1a977eb8cbf0794bcc2c7e1b326
Richard Gomes <rgomes.info@gmail.com>
2019-08-17 22:56:46 +0200

    install-minikube.sh

---
+++ sysadmin-install/install-minikube.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+function install_minikube {
+  [[ ! -d $HOME/Downloads ]] && mkdir -p $HOME/Downloads
+  pushd $HOME/Downloads
+
+  curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+    && sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+  virt-host-validate
+
+  curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 \
+    && sudo install docker-machine-driver-kvm2 /usr/local/bin/
+
+  popd
+}
+
+
+if [ $_ != $0 ] ;then
+  # echo "Script is being sourced"
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  # echo $dir
+  # echo $self
+  fgrep "function " $self | cut -d' ' -f2 | head -n -2
+else
+  # echo "Script is a subshell"
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  # echo $dir
+  # echo $self
+  cmd=$(fgrep "function " $self | cut -d' ' -f2 | head -n -2 | tail -1)
+  # echo $cmd
+  $cmd $*
+fi
