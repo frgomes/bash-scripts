ca8ade93054c6e4765e4ed0a64f8852b66463b31
Richard Gomes <rgomes.info@gmail.com>
2019-12-13 18:15:43 +0000

    Command for resetting terminal after IntelliJ screws it.

--- bash_50intellij.sh
+++ bash_50intellij.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+function intellij_reset_keyboard {
+  ibus-daemon -rd
+  sleep 3
+  stty sane
+  carpalx_hyena_gb
+}
+
 function intellij_clean {
   for base in $HOME/.IntelliJ* ;do
     for dir in caches jars js_caches jsp_related_caches compiler index compile-server ;do
