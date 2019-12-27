0487a8e9abe2613171aefb7bc6407ce388ec145f
Richard Gomes <rgomes.info@gmail.com>
2019-12-27 19:48:55 +0000

    Bugfix: should detect already existing symlink

--- bash_10aliases.sh
+++ bash_10aliases.sh
@@ -19,7 +19,7 @@ alias df='df -h'
 alias du='du -h'
 
 # the obligatory Emacs
-[[ -e /opt/emacs/bin/emacs ]] && ln -s /opt/emacs/bin/emacs ~/bin/emacs
+[[ -e /opt/emacs/bin/emacs ]] && [[ ! -e ~/bin/emacs ]] && ln -s /opt/emacs/bin/emacs ~/bin/emacs
 VISUAL=emacs
 EDITOR=emacs
 ALTERNATE_EDITOR=
