c5eb55911eb0e0f6dbf3686bd55eec24fc3a00da
Richard Gomes <rgomes.info@gmail.com>
2019-12-27 03:11:18 +0000

    Add support for Dotty

--- user-install/install-scala.sh
+++ user-install/install-scala.sh
@@ -4,12 +4,20 @@
 function install_scala_binaries {
   local version=${1:-"$SCALA_VERSION"}
   local version=${version:-"2.12.10"}
+  local major=$( echo ${version} | cut -d. -f 1-2 )
 
-  local file=scala-${version}.tgz
-  local url=http://downloads.lightbend.com/scala/${version}/${file}
-  local folder=scala-${version}
-  local symlink=scala
-
+  if [[ $version =~ ^(0\.).* ]] ;then
+    local file=dotty-${version}.tar.gz
+    local url=https://github.com/lampepfl/dotty/releases/download/${version}/${file}
+    local folder=dotty-${version}
+    local symlink=dotty-${major}
+  else
+    local file=scala-${version}.tgz
+    local url=http://downloads.lightbend.com/scala/${version}/${file}
+    local folder=scala-${version}
+    local symlink=scala-${major}
+  fi
+       
   local tools=${TOOLS_HOME:=$HOME/tools}
   local Software=${SOFTWARE_HOME:=/mnt/omv/Software}
 
@@ -33,18 +41,22 @@ function install_scala_binaries {
   
   if [ -L ${tools}/${symlink} ] ;then rm ${tools}/${symlink} ;fi
   ln -s ${folder} ${tools}/${symlink}
-
-  [[ ! -d ~/.bashrc-scripts/installed ]] && mkdir -p ~/.bashrc-scripts/installed
 }
 
 function install_scala_api {
   local version=${1:-"$SCALA_VERSION"}
   local version=${version:-"2.12.10"}
+  local major=$( echo ${version} | cut -d. -f 1-2 )
 
+  if [[ $version =~ ^(0\.).* ]] ;then
+    echo Dotty does not provide Scala API docs yet.
+    return
+  fi
+  
   local file=scala-docs-${version}.txz
   local url=http://downloads.lightbend.com/scala/${version}/${file}
   local folder=scala-${version}
-  local symlink=scala
+  local symlink=scala-${major}
 
   local tools=${TOOLS_HOME:=$HOME/tools}
   local Software=${SOFTWARE_HOME:=/mnt/omv/Software}
@@ -69,20 +81,22 @@ function install_scala_api {
   
   if [ -L ${tools}/${symlink} ] ;then rm ${tools}/${symlink} ;fi
   ln -s ${folder} ${tools}/${symlink}
-
-  [[ ! -d ~/.bashrc-scripts/installed ]] && mkdir -p ~/.bashrc-scripts/installed
 }
 
 function install_scala_spec {
   local version=${1:-"$SCALA_VERSION"}
   local version=${version:-"2.12.10"}
-
   local major=$( echo ${version} | cut -d. -f 1-2 )
 
+  if [[ $version =~ ^(0\.).* ]] ;then
+    echo Dotty does not provide a Scala spec yet.
+    return
+  fi
+  
   local file=scala-${version}.tgz
   local url=http://downloads.lightbend.com/scala/${version}/${file}
   local folder=scala-${version}
-  local symlink=scala
+  local symlink=scala-${major}
 
   local tools=${TOOLS_HOME:=$HOME/tools}
   local Software=${SOFTWARE_HOME:=/mnt/omv/Software}
@@ -96,14 +110,12 @@ function install_scala_spec {
 
   if [ -L ${tools}/${symlink} ] ;then rm ${tools}/${symlink} ;fi
   ln -s ${folder} ${tools}/${symlink}
-
 }
 
 function install_scala {
   install_scala_binaries $*
   install_scala_api $*
   install_scala_spec $*
-  [[ ! -d ~/.bashrc-scripts/installed ]] && mkdir -p ~/.bashrc-scripts/installed
   return 0
 }
         
