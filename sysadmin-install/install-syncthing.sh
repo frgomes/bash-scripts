07a1f6d8767b6d13b3d481210bc28ad51b9c3433
Richard Gomes <rgomes.info@gmail.com>
2019-12-31 16:23:02 +0000

    install-syncthing.sh

---
+++ sysadmin-install/install-syncthing.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# compiled from https://docs.docker.com/engine/installation/linux/debian/
+
+function install_syncthing_binaries {
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https ca-certificates wget software-properties-common
+
+    curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+    echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+    sudo apt-get update
+    sudo apt-get install syncthing
+}
+
+function install_syncthing {
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  grep -E "^function " $self | fgrep -v "function __" | cut -d' ' -f2 | head -n -1 | while read cmd ;do
+    $cmd $*
+  done
+}
+
+
+if [ $_ != $0 ] ;then
+  # echo "Script is being sourced: list all functions"
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  grep -E "^function " $self | fgrep -v "function __" | cut -d' ' -f2 | head -n -1
+else
+  # echo "Script is a subshell: execute last function"
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  cmd=$(grep -E "^function " $self | cut -d' ' -f2 | tail -1)
+  $cmd $*
+fi
