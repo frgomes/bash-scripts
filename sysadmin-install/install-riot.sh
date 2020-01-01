2e1f8bf0cb76bfc720903f6084b05573b4212dbd
Richard Gomes <rgomes.info@gmail.com>
2020-01-01 21:49:21 +0000

    Remove unwanted parenteses.

--- sysadmin-install/install-riot.sh
+++ sysadmin-install/install-riot.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-function install_riot_repository() {
+function install_riot_repository {
 sudo bash <<EOD
   echo "deb https://riot.im/packages/debian/ sid main" > /etc/apt/sources.list.d/matrix-riot-im.list
   wget -O - https://riot.im/packages/debian/repo-key.asc | sudo apt-key add -
 EOD
 }
 
-function install_riot_binaries() {
+function install_riot_binaries {
   sudo apt update && sudo apt install -y riot-web 
 }
 
