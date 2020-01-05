3bf35c1a5d25f44b432f99ea24cfa0f835cb46c7
Richard Gomes <rgomes.info@gmail.com>
2020-01-05 17:04:59 +0000

    Should try to find binaries in the storage first.

--- user-install/install-dbeaver.sh
+++ user-install/install-dbeaver.sh
@@ -3,21 +3,39 @@
 
 function install_dbeaver_binaries {
   local version=${1:-"$DBEAVER_VERSION"}
-  local version=${version:-"6.1.5"}
+  local version=${version:-"6.3.1"}
 
+  local file=dbeaver-ce-${version}-linux.gtk.x86_64.tar.gz
   local url=https://dbeaver.io/files/${version}/dbeaver-ce-${version}-linux.gtk.x86_64.tar.gz
-  local file=~/Downloads/dbeaver-ce-${version}-linux.gtk.x86_64.tar.gz
   local folder=dbeaver
-
-  [[ ! -d ~/Downloads ]] && mkdir -p ~/Downloads
-  [[ ! -f ${file} ]] && wget ${url} -O ${file}
+  local symlink=
 
   local tools=${TOOLS_HOME:=$HOME/tools}
+  local Software=${SOFTWARE_HOME:=/mnt/omv/Software}
 
-  [[ ! -d ${tools} ]] && mkdir -p $tools
-  [[ ! -d ${tools}/${folder} ]] 
-  tar -C ${tools} -xf ${file}
+  [[ ! -d ~/Downloads ]] && mkdir -p ~/Downloads
+  [[ ! -d $tools ]] && mkdir -p $tools
 
+  local archive=""
+  if [[ -f ${Software}/Linux/${file} ]] ;then
+    local archive=${Software}/Linux/${file}
+  elif [[ -f ${HOME}/Downloads/${file} ]] ;then
+    local archive=${HOME}/Downloads/${file}
+  fi
+  if [[ -z ${archive} ]] ;then
+    local archive=${HOME}/Downloads/${file}
+    wget "$url" -O "${archive}"
+  fi
+
+  if [ ! -d ${tools}/${folder} ] ;then
+    mkdir -p ${tools}/${folder}
+    tar -C ${tools}/${folder} --strip-components 1 -xpf ${archive}
+  fi
+  if [ ! -z ${symlink} ] ;then
+    if [ -L ${tools}/${symlink} ] ;then rm ${tools}/${symlink} ;fi
+    ln -s ${folder} ${tools}/${symlink}
+  fi
+  
   [[ ! -d ~/.bashrc-scripts/installed ]] && mkdir -p ~/.bashrc-scripts/installed
   cat << EOD > ~/.bashrc-scripts/installed/380-dbeaver.sh
 #!/bin/bash
