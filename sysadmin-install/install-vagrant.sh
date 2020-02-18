87f3815aaffb3bcb9c1de4712bba7afc79b2b30f
Richard Gomes <rgomes.info@gmail.com>
2020-02-18 12:02:57 +0000

    sysadmin-install/install-vagrant.sh

---
+++ sysadmin-install/install-vagrant.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+function install_vagrant_binaries {
+  sudo apt install vagrant vagrant-libvirt vagrant-digitalocean -y
+}
+
+function install_vagrant {
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  grep -E "^function " $self | fgrep -v "function __" | cut -d' ' -f2 | head -n -1 | while read cmd ;do
+    $cmd $*
+  done
+}
+
+
+if [ $_ != $0 ] ;then
+  # echo "Script is being sourced: list all functions"
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  grep -E "^function " $self | fgrep -v "function __" | cut -d' ' -f2 | head -n -1
+else
+  # echo "Script is a subshell: execute last function"
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  cmd=$(grep -E "^function " $self | cut -d' ' -f2 | tail -1)
+  $cmd $*
+fi
