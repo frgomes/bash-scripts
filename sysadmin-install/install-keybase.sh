09bcd19669d924ba78ab417a4c333f7ee9de733d
Richard Gomes <rgomes.info@gmail.com>
2020-02-20 13:33:46 +0000

    sysadmin-install/install-keybase.sh

---
+++ sysadmin-install/install-keybase.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -x
+
+function install_keybase_binaries {
+  local file=keybase_amd64.deb
+  local url=https://prerelease.keybase.io/${file}
+
+  local Software=${SOFTWARE_HOME:=/mnt/omv/Software}
+
+  [[ ! -d ~/Downloads ]] && mkdir -p ~/Downloads
+
+  local archive=""
+  if [[ -f ${Software}/Linux/Debian/${file} ]] ;then
+    local archive=${Software}/Linux/Debian/${file}
+  elif [[ -f ${HOME}/Downloads/${file} ]] ;then
+    local archive=${HOME}/Downloads/${file}
+  fi
+  if [[ -z ${archive} ]] ;then
+    local archive=${HOME}/Downloads/${file}
+    wget "$url" -O "${archive}"
+  fi
+
+  sudo apt install -y ${archive}
+}
+
+function install_keybase {
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  grep -E "^function " $self | fgrep -v "function __" | cut -d' ' -f2 | head -n -1 | while read cmd ;do
+    $cmd $*
+  done
+}
+
+
+if [ $_ != $0 ] ;then
+  # echo "Script is being sourced: list all functions"
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  grep -E "^function " $self | fgrep -v "function __" | cut -d' ' -f2 | head -n -1
+else
+  # echo "Script is a subshell: execute last function"
+  self=$(readlink -f "${BASH_SOURCE[0]}"); dir=$(dirname $self)
+  cmd=$(grep -E "^function " $self | cut -d' ' -f2 | tail -1)
+  $cmd $*
+fi
