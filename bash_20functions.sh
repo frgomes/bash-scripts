d6f95d4e2b6a694e44669ea793b05e03154243aa
Richard Gomes <rgomes.info@gmail.com>
2020-04-02 15:54:30 +0100

    json2yaml now keep original order (or disorder)

--- bash_20functions.sh
+++ bash_20functions.sh
@@ -172,5 +172,5 @@ function json_validate {
 }
 
 function json2yaml {
-  python -c 'import sys, yaml, json; print(yaml.dump(json.loads(sys.stdin.read())))'
+  python -c 'import sys, yaml, json; print(yaml.dump(json.loads(sys.stdin.read()), sort_keys=False))'
 }
