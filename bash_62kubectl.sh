f66874c936d9a8515118a671c1448b028de81842
Richard Gomes <rgomes.info@gmail.com>
2020-04-11 14:28:51 +0100

    Add kubectl_stats

--- bash_62kubectl.sh
+++ bash_62kubectl.sh
@@ -6,3 +6,7 @@ alias ky='kubectl -o yaml'
 function kubectl_clusters {
   cat ~/.kube/config | yaml2json | jq '.clusters[].name' | sed 's/"//g'
 }
+
+function kubectl_stats() {
+  kubectl get secret,configmap,statefulset,deployment,svc,ing,pods,pvc,pv $*
+}
