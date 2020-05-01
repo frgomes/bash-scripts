f0342da654189d3765a0ecac4473471d024028a2
Richard Gomes <rgomes.info@gmail.com>
2020-05-01 13:45:33 +0100

    Improving kubectl_stats

--- bash_62kubectl.sh
+++ bash_62kubectl.sh
@@ -7,6 +7,12 @@ function kubectl_clusters {
   cat ~/.kube/config | yaml2json | jq '.clusters[].name' | sed 's/"//g'
 }
 
-function kubectl_stats() {
-  kubectl get secret,configmap,statefulset,deployment,svc,ing,pods,pvc,pv $*
+function kubectl_stats {
+  local resources=$(kubectl api-resources --namespaced=true | cut -d' ' -f1 | tail -n +2 | sort | uniq | mkString)
+  kubectl get ${resources},pv $* 2> /dev/null
+}
+
+function kubectl_stats_cluster {
+  local resources=$(kubectl api-resources --namespaced=false | cut -d' ' -f1 | tail -n +2 | sort | uniq | mkString)
+  kubectl get ${resources} 2> /dev/null
 }
