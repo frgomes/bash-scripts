6721305f551b93d49ee52bd175cf93d3a2c2cb1b
Richard Gomes <rgomes.info@gmail.com>
2020-06-07 13:55:13 +0100

    yaml_split

--- bash_20functions.sh
+++ bash_20functions.sh
@@ -152,23 +152,3 @@ function _complete_workspace() {
   return 0
 }
 complete -F _complete_workspace workspace
-
-function yaml_validate {
-  python -c 'import sys, yaml, json; yaml.safe_load(sys.stdin.read())'
-}
-
-function yaml2json {
-  python -c 'import sys, yaml, json; print(json.dumps(yaml.safe_load(sys.stdin.read())))'
-}
-
-function yaml2json_pretty {
-  python -c 'import sys, yaml, json; print(json.dumps(yaml.safe_load(sys.stdin.read()), indent=2, sort_keys=False))'
-}
-
-function json_validate {
-  python -c 'import sys, yaml, json; json.loads(sys.stdin.read())'
-}
-
-function json2yaml {
-  python -c 'import sys, yaml, json; print(yaml.dump(json.loads(sys.stdin.read()), sort_keys=False))'
-}
