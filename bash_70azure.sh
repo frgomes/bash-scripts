56b608709151389e8affd296cf1345e10dbeab75
Richard Gomes <rgomes.info@gmail.com>
2021-03-19 01:04:49 +0000

    Remove unwanted files

--- bash_70azure.sh
+++
@@ -1,4 +0,0 @@
-### This is kept here for historical purposes.
-### We are now installing Azure CLI instead of using from a Docker container.
-###
-# alias azure-cli="docker run -it -v ${HOME}:/root azuresdk/azure-cli-python"
